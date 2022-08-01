99b0bfe0facaa2bf17025b5c812227d44da4522f
abraham <abrutrenyan049@gmail.com>
2022-07-31 23:45:45 -0300

    feat: solved every kata except 5, 14, and 16

--- homeworks/abraham-trenyan/06-js-basics-1/basics.js
+++ homeworks/abraham-trenyan/06-js-basics-1/basics.js
@@ -1,8 +1,8 @@
-//Katas 1: http://www.codewars.com/kata/opposite-number                  DONE
+//Katas 1: http://www.codewars.com/kata/opposite-number                  
 function opposite(number) {
   return number - number * 2;
 }
-// Katas 2: http://www.codewars.com/kata/basic-mathematical-operations    DONE
+// Katas 2: http://www.codewars.com/kata/basic-mathematical-operations    
 function basicOp(operation, number1, number2) {
   switch (operation) {
     case "+":
@@ -15,14 +15,14 @@ function basicOp(operation, number1, number2) {
       return number1 / number2;
   }
 }
-// Katas 3: http://www.codewars.com/kata/printing-array-elements-with-comma-delimiters    DONE
+// Katas 3: http://www.codewars.com/kata/printing-array-elements-with-comma-delimiters    
 function printArray(array) {
   let output = "";
   array.forEach((element) => (output += element + ","));
-  output = output.slice(0, -1);
+  output = output.slice(0, -1);                          // Deletes last comma
   return output;
 }
-// Katas 4: http://www.codewars.com/kata/transportation-on-vacation   DONE
+// Katas 4: http://www.codewars.com/kata/transportation-on-vacation   
 function rentalCarCost(d) {
   if (d < 3) return d * 40;
   else if (d < 7) return d * 40 - 20;
@@ -30,7 +30,7 @@ function rentalCarCost(d) {
 }
 // Katas 5: http://www.codewars.com/kata/calculating-with-functions
 
-// Katas 6: http://www.codewars.com/kata/get-the-middle-character   DONE
+// Katas 6: http://www.codewars.com/kata/get-the-middle-character   
 function getMiddle(s) {
   if (s.length % 2 == 0) {
     return s[s.length / 2 - 1] + s[s.length / 2];
@@ -38,24 +38,24 @@ function getMiddle(s) {
     return s[(s.length - 1) / 2];
   }
 }
-// Katas 7: http://www.codewars.com/kata/partition-on   DONE
+
+// Katas 7: http://www.codewars.com/kata/partition-on   
+
 function partitionOn(pred, items) {
-  let trueArr=[]
+  let trueArr=[];
   for(let i=0; i<items.length;i++){
     if(pred(items[i]) === true){
-      trueArr.push(items[i])
-      items.splice(i, 1)
+      trueArr.push(items[i]);
+      items.splice(i, 1);            // Deletes T elements
     }
   }
-  items.push(...trueArr);
-  return items.findIndex(pred)
+  items.push(...trueArr);            // Pushes T elements after the F's
+  return items.findIndex(pred);
 }
-function isEven(n) {return n % 2 == 0}
-var i = partitionOn(isEven, [1,2,3,4,5])
 
 // Katas 8: http://www.codewars.com/kata/word-count       URL is broken :(
 
-// Katas 9: https://www.codewars.com/kata/find-the-odd-int/               DONE
+// Katas 9: https://www.codewars.com/kata/find-the-odd-int/               
 function findOdd(A) {
   let count = 0;
   for (let i = 0; i < A.length; i++) {
@@ -70,7 +70,7 @@ function findOdd(A) {
     }
   }
 }
-// Katas 10: https://www.codewars.com/kata/find-the-parity-outlier          DONE
+// Katas 10: https://www.codewars.com/kata/find-the-parity-outlier          
 function findOutlier(integers) {
   let evenCount = 0;
   let oddCount = 0;
@@ -82,14 +82,14 @@ function findOutlier(integers) {
     }
   }
   if (evenCount >= 2) {
-    const outlier = integers.find((number) => number % 2 != 0);
+    let outlier = integers.find((number) => number % 2 != 0);
     return outlier;
   } else {
-    const outlier = integers.find((number) => number % 2 === 0);
+    let outlier = integers.find((number) => number % 2 === 0);
     return outlier;
   }
 }
-// Katas 11: https://www.codewars.com/kata/zipwith                            DONE
+// Katas 11: https://www.codewars.com/kata/zipwith                            
 function zipWith(fn, a0, a1) {
   let newArray = [];
   for (let i = 0; i < Math.min(a0.length, a1.length); i++) {
@@ -97,7 +97,7 @@ function zipWith(fn, a0, a1) {
   }
   return newArray;
 }
-// Katas 12: https://www.codewars.com/kata/filter-the-number                  DONE
+// Katas 12: https://www.codewars.com/kata/filter-the-number                  
 var FilterString = function (value) {
   let numbers = "";
   for (let i = 0; i < value.length; i++) {
@@ -107,7 +107,7 @@ var FilterString = function (value) {
   }
   return Number(numbers);
 };
-// Katas 13: https://www.codewars.com/kata/n-th-fibonacci                       DONE
+// Katas 13: https://www.codewars.com/kata/n-th-fibonacci                       
 function nthFibo(n) {
   let fibonacci = [0, 1];
   for (let i = 2; i <= n; i++) {
@@ -122,22 +122,64 @@ function nthFibo(n) {
 function duplicateEncode(word){
     lowered=word.toLowerCase();
     let newString = "";
-    for(let i=0;i<lowered.length;i++){
-      if(lowered.indexOf(lowered[i])===lowered.lastIndexOf(lowered[i])){
-        newString+='('
+    for(let i=0; i<lowered.length; i++){
+      if(lowered.indexOf(lowered[i])===lowered.lastIndexOf(lowered[i])){  // If the first and last time an element  
+        newString+='(';                                                   //appears is the same, its not duplicated
       }
-      else newString+=')'
+      else newString+=')';
     }
-    return newString
+    return newString;
 }
-console.log(duplicateEncode('abraham'))
-
 // Katas 16: https://www.codewars.com/kata/5693239fb761dc8670000001
 
 // Katas 17: https://www.codewars.com/kata/576757b1df89ecf5bd00073b
+function towerBuilder(nFloors) {
+  let tower = [];
+  for(i = 1; i <= nFloors ; i++){
+    let blanks = ' '.repeat(nFloors - i);  
+    let asteriscs = '*'.repeat(2 * i - 1);
+    let floor = `${blanks}${asteriscs}${blanks}`;
+    tower.push(floor);
+  }
+  return tower;
+} 
+// Katas 18: https://www.codewars.com/kata/58f5c63f1e26ecda7e000029               
+function wave(str){
+  waveArr=[];
+  if (str === ''){
+    return waveArr;
+  }
+  else{
+    for(let i=0; i<str.length; i++){
+      if (str[i] != ' '){
+          waveArr.push(str.slice(0,i) + str[i].toUpperCase()+str.slice(i+1)) //if we have a space we push the first
+        }                                                     //part, our capitalized letter, and rest of the string
+      else {
+        continue; 
+      }
+  }
+  return waveArr;
+  }
+}  
 
-// Katas 18: https://www.codewars.com/kata/58f5c63f1e26ecda7e000029
-
-// Katas 19: https://www.codewars.com/kata/59d398bb86a6fdf100000031
-
-// Katas 20: https://www.codewars.com/kata/514a024011ea4fb54200004b
+// Katas 19: https://www.codewars.com/kata/59d398bb86a6fdf100000031           
+function stringBreakers(n, string){
+  let stringWithoutSpaces = string.replace(/ /g, '');
+  let substring = '';  
+  let arr = [];
+  while(arr.length < n){
+      substring += stringWithoutSpaces.substring(0, n);
+      arr.push(substring);
+      substring='';      
+      stringWithoutSpaces = stringWithoutSpaces.substr(n, stringWithoutSpaces.length) //cut the taken chunk
+  }
+  return arr.join('\n');
+}
+// Katas 20: https://www.codewars.com/kata/514a024011ea4fb54200004b               
+  function domainName(url){
+    url=url.replace('http://', '')
+    url=url.replace('https://', '')
+    url=url.replace('www.', '')
+    return url.split('.')[0]
+  }
+ 
