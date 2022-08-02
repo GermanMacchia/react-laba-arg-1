6687a06e6b6c2212e594881889bd0358e5571f69
Candelaria Barcelo <barcelo.candelaria@gmail.com>
2022-08-02 17:47:27 -0300

    task 3: a long time ago

--- homeworks/candelaria-barcelo/08-js-advanced-1/index.js
+++ homeworks/candelaria-barcelo/08-js-advanced-1/index.js
@@ -17,3 +17,27 @@ const pluck = (object, keyInput) => {
 const clone = (obj) => {
   return JSON.parse(JSON.stringify(obj))
 }
+
+// task 3: a long time ago
+const offset = (dates) => {
+  const now = dates.pop()
+  let previousDate = dates.pop()
+  const previousArray = previousDate.split(/[/: ]/g)
+  previousDate = new Date (previousArray[2], previousArray[1]-1, previousArray[0], previousArray[3], previousArray[4], previousArray[5])
+  function msToTime(ms) {
+    let seconds = (ms / 1000).toFixed();
+    let minutes = (ms / (1000 * 60)).toFixed();
+    let hours = (ms / (1000 * 60 * 60)).toFixed();
+    let days = (ms / (1000 * 60 * 60 * 24)).toFixed();
+    if (seconds < 60) return seconds + " seconds ago";
+    else if (minutes < 60) return minutes + " minutes ago";
+    else if (hours < 24) return hours + " hours ago";
+    else return days + " days ago"
+  }
+  return (msToTime(now - previousDate))
+}
+const moment = (previousDate, format) => {
+  const now = new Date()
+  return [previousDate, now]
+}
+
