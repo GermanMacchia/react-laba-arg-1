a641203fa4c41d088b9dfcdd62925cebb7df13e2
Candelaria Barcelo <barcelo.candelaria@gmail.com>
2022-08-02 20:59:02 -0300

    task 1: error throwing - error handling #2

---
+++ homeworks/candelaria-barcelo/11-js-advanced-3/index.js
@@ -0,0 +1,10 @@
+// task 1 https://www.codewars.com/kata/55e7650c8d894146be000095/javascript
+function validateMessage(msg) {
+    if (msg === null) throw ReferenceError('Message is null!')
+    if (typeof msg !== "string") throw TypeError(`Message should be of type string but was of type ${typeof msg}!`)
+    if (msg.length > 255 || msg.length === 0) throw RangeError(`Message contains ${msg.length} characters!`)
+    if (msg.includes("<") && msg.includes(">")) return false
+    return true
+  }
+
+// task 2 
