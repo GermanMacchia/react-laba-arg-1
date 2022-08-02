09a9c68498929e4883a54665ac68c34adad84ec4
Candelaria Barcelo <barcelo.candelaria@gmail.com>
2022-08-02 17:17:15 -0300

    task 2: deep clone

--- homeworks/candelaria-barcelo/08-js-advanced-1/index.js
+++ homeworks/candelaria-barcelo/08-js-advanced-1/index.js
@@ -1,4 +1,4 @@
-// task 1 Pluck
+// task 1: Pluck function
 const pluck = (object, keyInput) => {
   try {
     let path = keyInput.split(".")
@@ -13,4 +13,21 @@ const pluck = (object, keyInput) => {
   }
 }
 
-// task 2
+// task 2: Deep clone
+
+const clone = (obj) => {
+  // const entries = Object.entries(obj)
+  // let clonedObject = {}
+  // for (let i=0; i < entries.length; i++) {
+  //   let value = entries[i][1]
+  //   while (typeof value === Object) {
+  //     const valueEntries = Object.entries(value)
+  //     for (let i=0; i < valueEntries.length; i++) {
+
+  //     }
+  //     value = value[]
+  //   }
+  //   clonedObject[entries[i][0]] = value
+  // }
+  return JSON.parse(JSON.stringify(obj))
+}
