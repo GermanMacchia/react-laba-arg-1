c49846b4e84721d12f33465890faccd61c28280d
Brisa Rojas <brojas@itba.edu.ar>
2022-08-03 09:03:07 -0300

    Fix: re-formated code

--- homeworks/brisa-rojas/08-js-advanced-1/task3.js
+++ homeworks/brisa-rojas/08-js-advanced-1/task3.js
@@ -42,12 +42,11 @@ console.log(pluck(nullValue, 'unknown.key')); // null
 console.log(pluck(user, 'username'));
 console.log(pluck(user, 'preferences.sound.maxValue'));
 
-
 // 2 - DEEP CLONE
 //Create custom deep clone function.
-function clone(obj){
-    return JSON.parse(JSON.stringify(obj)); 
-    // user obj is small enough to be cloned with JSON.parse/JSON.stringify approach
+function clone(obj) {
+  return JSON.parse(JSON.stringify(obj));
+  // user obj is small enough to be cloned with JSON.parse/JSON.stringify approach
 }
 
 let clonedUser = clone(user);
@@ -59,7 +58,7 @@ console.log(user.preferences.sound.maxValue === clonedUser.preferences.sound.max
 //Create a function that returns how long ago a certain day was.
 // 3 -  A LONG TIME AGO
 //Create a function that returns how long ago a certain day was.
-function offset(date){
+function offset(date) {
   const SECS_IN_DAY = 60 * 60 * 24;
   const SECS_IN_HOUR = 60 * 60;
   const SECS_IN_MIN = 60;
@@ -82,30 +81,30 @@ function offset(date){
   return format(offsetArray);
 }
 
-function format(array){
+function format(array) {
   let plural;
   let formatedString = '';
   let timeUnits = ['day', 'hour', 'minute', 'second'];
 
-  for (let i = 0; i < array.length; i++){
-      if (array[i] > 0){
-        plural = array[i] > 1 ? 's' : '';
-        formatedString += array[i] + ' ' + timeUnits[i] + plural  +  ' ';
-      }
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > 0) {
+      plural = array[i] > 1 ? 's' : '';
+      formatedString += array[i] + ' ' + timeUnits[i] + plural + ' ';
+    }
   }
   formatedString = formatedString + 'ago';
   return formatedString;
 }
 
-// Test 
-let date1 = new Date(Date.parse("Mar 17 2000, 10:42")); 
-console.log(offset(date1));  
-let date2 = new Date(Date.parse("Sept 29 1998")); 
-console.log(offset(date2));  
+// Test
+let date1 = new Date(Date.parse('Mar 17 2000, 10:42'));
+console.log(offset(date1));
+let date2 = new Date(Date.parse('Sept 29 1998'));
+console.log(offset(date2));
 
 // 4 - RANDOM DATES
 // Create a function that generate a random date between to dates.
-function randomDate(start, end) { 
+function randomDate(start, end) {
   let date = new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
   return date;
 }
@@ -113,32 +112,31 @@ function randomDate(start, end) {
 // Test
 console.log(randomDate(date1, date2));
 
-
 // 5 - MERGED OBJECTS https://www.codewars.com/kata/merged-objects
-function objConcat(arr){
+function objConcat(arr) {
   let obj = {};
-  for (let i = 0; i < arr.length; i++){
-    for (let key in arr[i]){
-      obj[key] = (arr[i])[key];
+  for (let i = 0; i < arr.length; i++) {
+    for (let key in arr[i]) {
+      obj[key] = arr[i][key];
     }
   }
   return obj;
 }
 
-// 6 - NAMED ONE https://www.codewars.com/kata/547f1a8d4a437abdf800055c/train/javascript 
+// 6 - NAMED ONE https://www.codewars.com/kata/547f1a8d4a437abdf800055c/train/javascript
 function NamedOne(first, last) {
   this.firstName = first;
   this.lastName = last;
 
-  Object.defineProperty(this, "fullName", {
+  Object.defineProperty(this, 'fullName', {
     get: function () {
-      return this.firstName + " " + this.lastName;
+      return this.firstName + ' ' + this.lastName;
     },
     set(full) {
-      if (full.includes(' ')){
+      if (full.includes(' ')) {
         full = full.split(' ');
-        this.firstName =  full[0];
-        this.lastName =  full [1];
+        this.firstName = full[0];
+        this.lastName = full[1];
         full = full.join(' ');
       }
     },
@@ -157,7 +155,6 @@ console.log(person.fullName); // I used a Multi Word String
 person.fullName = 'Disaster happens when given a 3 words or longer name for full name';
 console.log(person.fullName); // Disaster happens [when given a 3 words or longer name for full name is missing]
 
-
 // OPTIONAL KATAS
 // "THIS" IS AN OTHER SOLUTION https://www.codewars.com/kata/54834b3559e638b39d0009a2/javascript
 function OnceNamedOne(first, last) {
@@ -168,46 +165,41 @@ function OnceNamedOne(first, last) {
 }
 
 // HUMAN READABLE TIME https://www.codewars.com/kata/human-readable-time
-function humanReadable (seconds) {
-  let humanReadableTime= '';
+function humanReadable(seconds) {
+  let humanReadableTime = '';
   let hours;
   let minutes;
-  
+
   //set hours
-  hours = Math.floor(seconds/3600);
-  if (hours < 10){
-      humanReadableTime += '0' + hours;
-  }
-  else {
-      humanReadableTime += hours;
+  hours = Math.floor(seconds / 3600);
+  if (hours < 10) {
+    humanReadableTime += '0' + hours;
+  } else {
+    humanReadableTime += hours;
   }
 
-  seconds -= hours * 3600; // remove seconds corresponding to hours 
-  
+  seconds -= hours * 3600; // remove seconds corresponding to hours
+
   humanReadableTime += ':'; // formating
 
   //set minutes
-  minutes = Math.floor(seconds/60);
-  if (minutes < 10){
-      humanReadableTime += '0' + minutes;
+  minutes = Math.floor(seconds / 60);
+  if (minutes < 10) {
+    humanReadableTime += '0' + minutes;
+  } else {
+    humanReadableTime += minutes;
   }
-  else {
-      humanReadableTime += minutes;
-  }
-  seconds -= minutes * 60; // remove seconds corresponding to minutes 
-  
+  seconds -= minutes * 60; // remove seconds corresponding to minutes
+
   humanReadableTime += ':'; // formating
 
-  if (seconds < 10){
-      humanReadableTime += '0' + seconds;
-  }
-  else {
-      humanReadableTime += seconds;
+  if (seconds < 10) {
+    humanReadableTime += '0' + seconds;
+  } else {
+    humanReadableTime += seconds;
   } //remaining should be below 60 secs
 
   return humanReadableTime;
 }
 
-console.log(humanReadable(360)); 
-
-
+console.log(humanReadable(360));
