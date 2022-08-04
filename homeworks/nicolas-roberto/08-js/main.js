b35b9c775baf308a5ae47e4b4cc0f519f468449b
Nicolás <nicolaseugroberto@gmail.com>
2022-08-04 12:45:25 -0300

    Corrected branch name

--- homeworks/nicolas-roberto/08-js/main.js
+++ homeworks/nicolas-roberto/08-js/main.js
@@ -56,7 +56,7 @@ clonedUser.preferences.sound.maxValue = 70;
 console.log(user.preferences.sound.maxValue === clonedUser.preferences.sound.maxValue); // false
 
 // kata 4 Random dates
-
+//Work in progress, for some reason i still can figure out, it gets random dates up to march.
 function randomDate(startDate, endDate) {
   const minValue = startDate.getTime();
   const maxValue = endDate.getTime();
@@ -70,16 +70,25 @@ console.log(randomDate(new Date(2021, 1, 23), new Date(2021, 2, 23)).toLocaleDat
 
 function objConcat(array) {
   let merged = {};
-  for (object of array) {
+  //Makes a loop for each object in the array
+  array.forEach((object) => {
+    //and inside makes a loop for every key in the object
     for (keys in object) {
+      //Merged will have a key for every key in the original array
+      //So the keys will remain the same, but the new values will
+      //get replaced in each loop with the new/repeated values.
       merged[keys] = object[keys];
     }
-  }
+  });
   return merged;
 }
-
+//Test
 let object = [
   (a = { 1: '1', 2: '2', 3: '3' }),
   (b = { 3: '4', 5: '6', 6: '7', 7: '8' }),
   (c = { 5: '9', 8: '9', 6: '12', 23: '35' }),
 ];
+
+console.log(objConcat(object));
+
+// kata 6 "this" is an other problem https://www.codewars.com/kata/547f1a8d4a437abdf800055c/train/javascript
