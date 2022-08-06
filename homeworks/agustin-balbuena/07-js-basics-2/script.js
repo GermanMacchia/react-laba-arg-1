3d35f500ae023043705f8332c8bc62113640e774
Agustin Balbuena <ag.balbuena91@gmail.com>
2022-08-06 18:32:08 -0300

    katas 1, 2

--- homeworks/agustin-balbuena/07-js-basics-2/script.js
+++ homeworks/agustin-balbuena/07-js-basics-2/script.js
@@ -11,27 +11,11 @@ function positiveSum(arr) {
 
 // 2 -> https://www.codewars.com/kata/5a3e1319b6486ac96f000049
 function pairs(ar) {
-  let counter = 0;
-  let actualChar = null;
-  let lastChar = '';
-  let consecutives = 0;
-  for (let i = 0; i < ar.length; i++) {
-    actualChar = ar[i];
-
-    if (actualChar === lastChar + 1 || actualChar === lastChar - 1) {
-      //they're consecutives
-      consecutives++;
-    }
-
-    counter++;
-
-    lastChar = ar[i];
-
-    if (counter === 2) {
-      counter = 0;
-      actualChar = null;
-      lastChar = '';
+  let sequence = 0;
+  for (let i = 0; i < ar.length; i += 2) {
+    if (ar[i] === ar[i + 1] + 1 || ar[i] === ar[i + 1] - 1) {
+      sequence++;
     }
   }
-  return consecutives;
+  return sequence;
 }
