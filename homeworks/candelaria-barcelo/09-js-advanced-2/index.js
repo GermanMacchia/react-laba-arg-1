3e2cc286c879a81ea2b62ea3cb66707904bcaa5d
Candelaria Barcelo <barcelo.candelaria@gmail.com>
2022-08-07 19:27:55 -0300

    adding support for other data types

--- homeworks/candelaria-barcelo/09-js-advanced-2/index.js
+++ homeworks/candelaria-barcelo/09-js-advanced-2/index.js
@@ -1,39 +1,43 @@
-let dateVariables = []
+// my code
+let specialVariables = {
+  dates: [],
+  infinity: [],
+  minusInfinity: [],
+  nan: [],
+}
 
 class Serializable {
     serialize() {
-        console.log("json stringify", JSON.stringify(this) + "%" + this.constructor.name)
         const objEntries = Object.entries(this)
         for (let i=0; i < objEntries.length; i++) {
-            //console.log(objEntries)
-            if (objEntries[i][1] instanceof Date) {
-                console.log(objEntries[i][1], objEntries[i][0])
-                dateVariables.push(objEntries[i][0])
-            }
+            if (objEntries[i][1] instanceof Date) { specialVariables["dates"].push(objEntries[i][0]) }
+            if (objEntries[i][1] === Infinity) { specialVariables["infinity"].push(objEntries[i][0]) }
+            if (objEntries[i][1] === -Infinity) { specialVariables["minusInfinity"].push(objEntries[i][0]) }
+            if (isNaN(objEntries[i][1]) && typeof objEntries[i][1] !== "string" && typeof objEntries[i][1] !== "object") { 
+              specialVariables["nan"].push(objEntries[i][0]) }
         }
         return JSON.stringify(this) + "%" + this.constructor.name
     }
 
     wakeFrom(serialized) {
-        //console.log("json parse", JSON.parse(serialized))
-        //console.log("this", this)
-        
         const serializedClass = serialized.split("%")[1]
         const obj = JSON.parse(serialized.split("%")[0])
-        //console.log(serializedClass, this.constructor.name)
 
         if (serializedClass === this.constructor.name) {
             const entries = Object.entries(obj)
-            //console.log(entries)
             for (let i=0; i < entries.length; i++) {
-                //console.log("entry name", entries[i][0])
-                //console.log("current entry value", this[entries[i][0]])
-                //console.log("value name", entries[i][1])
-                if (dateVariables.includes(entries[i][0])) {
+              let currentEntry = entries[i][0]
+                if (specialVariables["dates"].includes(currentEntry)) {
                     const isoDate = entries[i][1].substring(0, 10)
-                    this[entries[i][0]] = new Date(isoDate)
+                    this[currentEntry] = new Date(isoDate)
+                } else if (specialVariables["infinity"].includes(currentEntry)) {
+                  this[currentEntry] = Infinity
+                } else if (specialVariables["minusInfinity"].includes(currentEntry)) {
+                  this[currentEntry] = -Infinity
+                } else if (specialVariables["nan"].includes(currentEntry)) {
+                  this[currentEntry] = NaN
                 } else {
-                    this[entries[i][0]] = entries[i][1]
+                    this[currentEntry] = entries[i][1]
                 }
             } 
             return this
@@ -42,18 +46,24 @@ class Serializable {
 }
 
 
-
-
-
-
+// code from the task
 class UserDTO extends Serializable {
-    constructor({ firstName, lastName, phone, birth } = {}) {
+    constructor({ firstName, lastName, phone, birth, age, money, wants, 
+                  hasDog, favouriteNumber, nan, shoppingList, superpowers } = {}) {
       super();
   
       this.firstName = firstName;
       this.lastName = lastName;
       this.phone = phone;
       this.birth = birth;
+      this.age = age;
+      this.money = money;
+      this.wants = wants;
+      this.hasDog = hasDog;
+      this.favouriteNumber = favouriteNumber;
+      this.nan = nan;
+      this.shoppingList = shoppingList;
+      this.superpowers = superpowers;
     }
   
     printInfo() {
@@ -66,21 +76,29 @@ class UserDTO extends Serializable {
     lastName: 'Nashovich',
     phone: '2020327',
     birth: new Date('1999-01-02'),
+    age: 54,
+    money: -Infinity,
+    wants: Infinity,
+    hasDog: null,
+    favouriteNumber: -0,
+    nan: NaN,
+    shoppingList: ["apples", "oranges"],
+    superpowers: {
+      power1: "flying",
+      power2: "invisibility"
+    }
   });
   
   tolik.printInfo(); //A. Nashovich - 2020327, 1999-01-02T00:00:00.000Z
-  //console.log(tolik)
 
   const serialized = tolik.serialize();
   tolik = null
-  //console.log(serialized)
-  //console.log(tolik)
   
   const resurrectedTolik = (new UserDTO()).wakeFrom(serialized);
-  //console.log(resurrectedTolik)
+  console.log(resurrectedTolik)
   
   console.log(resurrectedTolik instanceof UserDTO); // true
-  console.log(resurrectedTolik.printInfo()); // A. Nashovich - 2020327, 1999-01-02T00:00:00.000Z
+  //console.log(resurrectedTolik.printInfo()); // A. Nashovich - 2020327, 1999-01-02T00:00:00.000Z
   
   class Post extends Serializable {
     constructor({ content, date, author } = {}) {
