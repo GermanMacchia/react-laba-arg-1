744ef240e358fb123d9dd8549af091da3dbeca5f
Agustin Balbuena <ag.balbuena91@gmail.com>
2022-08-07 00:53:24 -0300

    katas 9

--- homeworks/agustin-balbuena/07-js-basics-2/script.js
+++ homeworks/agustin-balbuena/07-js-basics-2/script.js
@@ -84,3 +84,18 @@ function gimme(triplet) {
 const binaryArrayToNumber = (arr) => {
   return parseInt(arr.join(''), 2);
 };
+
+// 9 -> https://www.codewars.com/kata/585d7d5adb20cf33cb000235
+function findUniq(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === arr[i + 1]) {
+      continue;
+    } else {
+      if (arr[i + 1] !== arr[i + 2]) {
+        return arr[i + 1];
+      } else {
+        return arr[i];
+      }
+    }
+  }
+}
