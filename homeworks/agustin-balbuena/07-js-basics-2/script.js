e921f751c0396817240ee45da666d76dc55fbeeb
Agustin Balbuena <ag.balbuena91@gmail.com>
2022-08-07 00:26:43 -0300

    katas 7

--- homeworks/agustin-balbuena/07-js-basics-2/script.js
+++ homeworks/agustin-balbuena/07-js-basics-2/script.js
@@ -72,3 +72,10 @@ function evenChars(string) {
 
   return newString.split('');
 }
+
+// 7 -> https://www.codewars.com/kata/545a4c5a61aa4c6916000755
+function gimme(triplet) {
+  newTriplet = [...triplet];
+  let middle = triplet.sort((a, b) => a - b)[1];
+  return newTriplet.indexOf(middle);
+}
