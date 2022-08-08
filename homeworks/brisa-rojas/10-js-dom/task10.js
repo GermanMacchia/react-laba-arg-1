0290bcf6e7fe2e04d1cc64f56430e3dfa28507c9
Brisa Rojas <brojas@itba.edu.ar>
2022-08-07 22:46:36 -0300

    Deleted some comments and formatted

--- homeworks/brisa-rojas/10-js-dom/task10.js
+++ homeworks/brisa-rojas/10-js-dom/task10.js
@@ -11,40 +11,37 @@ for (let i = 1; i <= NUMBER_OF_ROWS; i++) {
     cell.className = 'cell';
     cell.setAttribute('row', i);
     cell.setAttribute('col', j);
-    //cell.innerText = `x:${i}\ny:${j}`;
     document.body.append(cell);
   }
 }
 
 // add event listener to body (Event Delegation approach)
 document.body.addEventListener('click', function (event) {
-    let target = event.target;
-    // get the row and column number of the clicked cell
-    let row = target.getAttribute('row');
-    let col = target.getAttribute('col');
-    // get the cell that has the same row and column number of the clicked cell
-    let sameColCells = document.querySelectorAll(`[col="${col}"]`);
-    let sameRowCells = document.querySelectorAll(`[row="${row}"]`);
+  let target = event.target;
+  // get the row and column number of the clicked cell
+  let row = target.getAttribute('row');
+  let col = target.getAttribute('col');
+  // get the cell that has the same row and column number of the clicked cell
+  let sameColCells = document.querySelectorAll(`[col="${col}"]`);
+  let sameRowCells = document.querySelectorAll(`[row="${row}"]`);
 
-    if (target.className != 'cell') {
-        return;
-    }
-    
-    // reset previous state
-    document.querySelectorAll(`.cell`).forEach(cell => { 
-        cell.style.backgroundColor = 'white'; 
-        cell.innerText = '';
-    });
+  if (target.className != 'cell') {
+    return;
+  }
 
-    // change background color
-    sameRowCells.forEach(function (cell) {
-        cell.style.backgroundColor = 'lightblue';
-    })
-    sameColCells.forEach(function (cell) {
-        cell.style.backgroundColor = 'lightblue';
-    });
-    target.style.backgroundColor = 'blue';
-    target.innerText = `x:${row}\ny:${col}`;
+  // reset previous state
+  document.querySelectorAll(`.cell`).forEach((cell) => {
+    cell.style.backgroundColor = 'white';
+    cell.innerText = '';
+  });
 
+  // change background color
+  sameRowCells.forEach(function (cell) {
+    cell.style.backgroundColor = 'lightblue';
+  });
+  sameColCells.forEach(function (cell) {
+    cell.style.backgroundColor = 'lightblue';
+  });
+  target.style.backgroundColor = 'blue';
+  target.innerText = `x:${row}\ny:${col}`;
 });
-
