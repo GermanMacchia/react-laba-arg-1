ffe904e945c0c58e3753ba5ba298b6560855cbe2
abraham <abrutrenyan049@gmail.com>
2022-08-08 14:44:10 -0300

    add solutions for most katas

--- homeworks/abraham-trenyan/07-js-basics-2/script.js
+++ homeworks/abraham-trenyan/07-js-basics-2/script.js
@@ -1,4 +1,4 @@
-// Kata 1: https://www.codewars.com/kata/5715eaedb436cf5606000381/train/javascript                 DONE
+// Kata 1: https://www.codewars.com/kata/5715eaedb436cf5606000381/train/javascript                 
 function positiveSum(arr) {
   let sum = 0;
   arr.forEach(number => {
@@ -8,7 +8,7 @@ function positiveSum(arr) {
   });
   return sum;
 }
-// Kata 2: https://www.codewars.com/kata/5a3e1319b6486ac96f000049/train/javascript                  DONE
+// Kata 2: https://www.codewars.com/kata/5a3e1319b6486ac96f000049/train/javascript                  
 function pairs(ar){
     let count = 0;
     for(i = 0; i < ar.length; i+=2){
@@ -18,7 +18,7 @@ function pairs(ar){
     }
     return count;
 };
-// Kata 3: https://www.codewars.com/kata/5aba780a6a176b029800041c/train/javascript                     DONE
+// Kata 3: https://www.codewars.com/kata/5aba780a6a176b029800041c/train/javascript                     
 function maxMultiple(divisor, bound){
     let largestInt = null;
     for(i = 1; i<=bound; i++){
@@ -28,13 +28,13 @@ function maxMultiple(divisor, bound){
     }
     return largestInt;
 }
-// Kata 4: https://www.codewars.com/kata/514a6336889283a3d2000001/train/javascript                      DONE
+// Kata 4: https://www.codewars.com/kata/514a6336889283a3d2000001/train/javascript                      
 let fiteredArray = []
 function getEvenNumbers(numbersArray){
     filteredArray = numbersArray.filter(n => n % 2 == 0);
     return filteredArray;
 }  
-// Kata 5: https://www.codewars.com/kata/5a090c4e697598d0b9000004                                       DONE
+// Kata 5: https://www.codewars.com/kata/5a090c4e697598d0b9000004                                       
 function solve(arr){
     let newArray = [];
     for(i=0; i<=arr.length; i++){
@@ -47,7 +47,7 @@ function solve(arr){
     newArray.push(...arr);
     return newArray;
 };
-// Kata 6: https://www.codewars.com/kata/566044325f8fddc1c000002c/train/javascript              DONE
+// Kata 6: https://www.codewars.com/kata/566044325f8fddc1c000002c/train/javascript              
 function evenChars(string) {
     let evenCharacters = []
     if(string.length>100 || string.length<2){
@@ -60,24 +60,48 @@ function evenChars(string) {
         return evenCharacters;
     }
 }
-// Kata 7: https://www.codewars.com/kata/545a4c5a61aa4c6916000755/train/javascript                  DONE
+// Kata 7: https://www.codewars.com/kata/545a4c5a61aa4c6916000755/train/javascript                  
 function gimme (triplet) {
     let sortedArr = [...triplet].sort(function(a, b) {return a - b;});
     return triplet.indexOf(sortedArr[1]);
 }
 // Kata 8: https://www.codewars.com/kata/578553c3a1b8d5c40300037c
 const binaryArrayToNumber = arr => {
-  };
+    let sum = 0;
+    let reversedArr = arr.reverse();          // Reverts the order so it can take it to the power of the normal
+    for(i=0; i<reversedArr.length; i++){        // index, instead of counting indexes backwards as in this method
+        if(arr[i]==1){                                      
+            sum+=(Math.pow(2, i))      
+        }
+    }
+    return sum;
+};
 
-// Kata 9: https://www.codewars.com/kata/585d7d5adb20cf33cb000235
+// Kata 9: https://www.codewars.com/kata/585d7d5adb20cf33cb000235                           
 function findUniq(arr) {
+    for(i=0; i<arr.length; i++){
+        if(arr.indexOf(arr[i])==arr.lastIndexOf(arr[i])){
+            return arr[i];
+           }}
 }
 // Kata 10: https://www.codewars.com/kata/581e014b55f2c52bb00000f8/train/javascript
 function decipherThis(str) {
-}; 
+}
 // Kata 11: https://www.codewars.com/kata/578aa45ee9fd15ff4600090d/train/javascript
-function sortArray(array) {
-    // Return a sorted array.
+function sortArray(arr) {
+    for(let i = 0; i<arr.length; i++){
+        if(arr[i]%2 != 0){
+            for(let j = i+1; j<arr.length; j++){
+                if(arr[i]>arr[j] && arr[j]%2!=0){
+                    let aux = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = aux;
+                    console.log(arr);
+                }
+            }
+        }
+    }
+    return arr;
 }
 
 //Optional 1: https://www.codewars.com/kata/515bb423de843ea99400000a
