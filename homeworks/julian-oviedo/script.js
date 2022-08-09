0f73596988958e7d54c13f7799267798e5903c7c
JulianOvie <ocotemc@gmail.com>
2022-08-09 00:05:35 -0300

    feat: task 10 without holding shift tool

---
+++ homeworks/julian-oviedo/script.js
@@ -0,0 +1,31 @@
+const container = document.querySelector('.grid-container');
+
+for (let i = 1; i <= 30; i++) {
+  for (let j = 1; j <= 20; j++) {
+    let div = document.createElement('div');
+    div.setAttribute('row', i);
+    div.setAttribute('col', j);
+    div.className = 'cell';
+    container.append(div);
+  }
+}
+
+const allCells = document.querySelectorAll('div');
+const allCellsArr = [...allCells];
+
+function clickHandler(e) {
+  const clicked = e.target;
+  if (clicked.matches('.cell')) {
+    allCellsArr.forEach((e) => {
+      e.className = 'cell';
+      e.innerText = '';
+    });
+    clicked.classList.toggle('cellOver');
+    const rowNum = clicked.getAttribute('row');
+    const colNum = clicked.getAttribute('col');
+    clicked.innerText = `x : ${rowNum} 
+    y : ${colNum}`;
+  }
+}
+
+container.addEventListener('click', clickHandler);
