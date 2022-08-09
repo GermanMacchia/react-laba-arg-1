4a8cf4707fed3a91b3eaba1c65aaec1cf1cbb740
JulianOvie <ocotemc@gmail.com>
2022-08-09 00:17:33 -0300

    chore: some comments to explain the logic

--- homeworks/julian-oviedo/script.js
+++ homeworks/julian-oviedo/script.js
@@ -12,10 +12,13 @@ for (let i = 1; i <= 30; i++) {
 
 const allCells = document.querySelectorAll('div');
 const allCellsArr = [...allCells];
+// turn to an array the nodeList, to apply forEach in the clickHandler function
 
 function clickHandler(e) {
   const clicked = e.target;
+  // only allows the event to divs created
   if (clicked.matches('.cell')) {
+    // clean styles before a new click
     allCellsArr.forEach((e) => {
       e.className = 'cell';
       e.innerText = '';
