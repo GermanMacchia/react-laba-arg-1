b5af0ef94e3869ee7623ba3a2e068db2a5ccdeda
Nicolás <nicolaseugroberto@gmail.com>
2022-08-11 20:19:57 -0300

    First kata solved, with commentary

---
+++ homeworks/nicolas-roberto/11-js-advanced-3/codewars.js
@@ -0,0 +1,19 @@
+// Kata 1 - Error Throwing - Error Handling #2 https://www.codewars.com/kata/55e7650c8d894146be000095/train/javascript
+
+const validateMessage = (msg) => {
+  //checks if the message is "empty"
+  if (msg == null) {
+    throw new ReferenceError('Message is null!');
+    //Checks if the message is a string
+  } else if (typeof msg != 'string') {
+    throw new TypeError(`Message should be of type string but was of type ${typeof msg}!`);
+    // Checks if the message length is equal to 0 or over 255
+  } else if (msg.length === 0 || msg.length > 255) {
+    throw new RangeError(`Message contains ${msg.length} characters!`);
+    // Checks if the message contains HTML-tag ( "<" or ">")
+  } else if (msg.includes('<') && msg.includes('>')) {
+    return false;
+  }
+  // Returns true at the end, if it's a valid string.
+  return true;
+};
