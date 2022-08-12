499450ccd01989675a35d481ff24108d88e9947e
Federico Benitez <fedebenitez1696@gmail.com>
2022-08-11 21:26:20 -0300

    exercise 2

--- homeworks/federico-benitez/07-js-basics-2/index.js
+++ homeworks/federico-benitez/07-js-basics-2/index.js
@@ -10,3 +10,21 @@ function positiveSum(arr) {
     return prev + 0;
   }, 0);
 }
+
+/**
+ * Exercise 2
+ * https://www.codewars.com/kata/5a3e1319b6486ac96f000049
+ */
+function pairs(arr) {
+  let count = 0;
+  arr.reduce((prev, curr, currIndex) => {
+    if (currIndex > 0 && currIndex % 2 !== 0) {
+      if (prev === curr - 1 || prev === curr + 1) {
+        count++;
+      }
+    }
+    return curr;
+  }, 0);
+
+  return count;
+}
