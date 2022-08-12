1dbe8a0349ff7e9ed0180df3c9acb7110cfa36e2
dazuaje <davidazuaje1084@gmail.com>
2022-08-12 19:09:39 -0300

    Task 8

--- homeworks/david-azuaje/08-js-advanced-1/script.js
+++ homeworks/david-azuaje/08-js-advanced-1/script.js
@@ -17,4 +17,29 @@ const uuser = {
     uuser.preferences.sound.maxValue === clonedUser.preferences.sound.maxValue
 );
 // false
+
+//Optional
+//Task 8) => https://www.codewars.com/kata/54834b3559e638b39d0009a2
+
+class OnceNamedOne {
+    constructor(firstName, lastName) {
+      this.first = firstName;
+      this.last = lastName;
+    }
+  
+    get fullName() {
+      return `${this.first} ${this.last}`;
+    }
+  
+    get firstName() {
+      return this.first;
+    }
+  
+    get lastName() {
+      return this.last;
+    }
+  }
+  
+const users = new OnceNamedOne("David", "Azuaje");
+console.log(users.fullName);
   
