53121a181b357e741c99f0561486cc4ddd56217c
German O. Macchia <germanmacchia@gmail.com>
2022-08-12 22:56:23 -0300

    task advanced features

--- homeworks/german-macchia/13-environment-and-preprocessors/webpack.config.js
+++ homeworks/german-macchia/13-environment-and-preprocessors/webpack.config.js
@@ -1,13 +1,40 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   entry: "./src/index.js",
   mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  devtool: 'source-map',
   module: {
     rules: [
       {
         test: /\.(css|sass|scss)$/,
         use: ["style-loader", "css-loader", "sass-loader"],
       },
+      {
+        test:/\.(png|jpg|jpeg|gif)$/i,
+        type: 'asset/source',
+        loader: 'file-loader',
+        options: {
+          name: './images/[name].[ext]',
+        },
+      },
+      {
+        test:/\.svg$/i,
+        type: 'asset/source',
+        loader: 'file-loader',
+        options: {
+          name: './images/icons/[name].[ext]',
+        },
+      },
     ],
-  }
+  },
+  plugins: [new HtmlWebpackPlugin({
+    filename: 'index.html',
+    template: './public/index.html'
+  })],
 };
