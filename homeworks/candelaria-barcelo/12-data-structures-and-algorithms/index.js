6d935ebfd8f14564498615e5f69f1ad9bc5ee957
Candelaria Barcelo <barcelo.candelaria@gmail.com>
2022-08-13 21:35:20 -0300

    adding binary search

--- homeworks/candelaria-barcelo/12-data-structures-and-algorithms/index.js
+++ homeworks/candelaria-barcelo/12-data-structures-and-algorithms/index.js
@@ -1,27 +1,46 @@
 requirejs(['./MOCK_DATA'], function (importedData) {
   const orderedData = importedData.data.sort((a, b) => (a.sku > b.sku ? 1 : b.sku > a.sku ? -1 : 0));
-  console.log(orderedData[0].sku < orderedData[1].sku);
   const output = document.getElementById('output');
 
   const straightClickHandler = () => {
     let input = document.getElementById('searchbox').value.toLowerCase();
     if (!input) return;
-    const result = orderedData.filter((item) => item.sku === input)[0];
-    output.innerText = `Name: ${result.name} \n
-    Pack: ${result.pack} \n
-    Price: ${result.price} \n
-    Sku: ${result.sku}`;
+    let linearResult = '';
+    for (let i = 0; i < orderedData.length; i++) {
+      if (orderedData[i].sku === input) {
+        linearResult = orderedData[i];
+      }
+    }
+    output.innerText = `Name: ${linearResult.name} \n
+    Pack: ${linearResult.pack} \n
+    Price: ${linearResult.price} \n
+    Sku: ${linearResult.sku}`;
   };
 
   const binaryClickHandler = () => {
-    // let input = document.getElementById('searchbox').value.toLowerCase();
-    // if (!input) return;
-    // let middlePoint = orderedData[Math.floor(orderedData.length / 2)];
-    // if (input < middlePoint.sku) {
-    // } else if (input > middlePoint.sku) {
-    // } else if (input === middlePoint.sku) {
-    //     return
-    // }
+    let input = document.getElementById('searchbox').value.toLowerCase();
+    if (!input) return;
+    let middlePoint = orderedData[Math.floor(orderedData.length / 2)];
+    let binaryResult = '';
+    if (input < middlePoint.sku) {
+      for (let i = 0; i < Math.floor(orderedData.length / 2); i++) {
+        if (orderedData[i].sku === input) {
+          binaryResult = orderedData[i];
+        }
+      }
+    } else if (input > middlePoint.sku) {
+      for (let i = Math.floor(orderedData.length / 2 + 1); i < orderedData.length; i++) {
+        if (orderedData[i].sku === input) {
+          binaryResult = orderedData[i];
+        }
+      }
+    } else if (input === middlePoint.sku) {
+      binaryResult = middlePoint;
+    }
+    output.innerText = `Name: ${binaryResult.name} \n
+    Pack: ${binaryResult.pack} \n
+    Price: ${binaryResult.price} \n
+    Sku: ${binaryResult.sku}`;
   };
 
   document.getElementById('straight-search-btn').addEventListener('click', straightClickHandler);
