9cdc11cf01e1f643380e59492612d01408d79010
Nicolás <nicolaseugroberto@gmail.com>
2022-08-14 02:20:37 -0300

    Unsolved kata 2

--- homeworks/nicolas-roberto/11-js-advanced-3/codewars.js
+++ homeworks/nicolas-roberto/11-js-advanced-3/codewars.js
@@ -20,4 +20,23 @@ const validateMessage = (msg) => {
 
 // Kata 2 - Jokes you've been 'awaiting' for ... promise https://www.codewars.com/kata/5a353a478f27f244a1000076
 
-async function sayJoke(apiUrl, jokeId) {}
+async function sayJoke(apiUrl, jokeId) {
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+
+  if (!data) {
+    throw new Error('No jokes at url: ${apiUrl}');
+  }
+  const joke = data.jokes.find((obj) => obj.id === jokeId);
+  if (!joke) {
+    throw new Error('No jokes found id: ${jokeId}');
+  }
+  return {
+    saySetup: function () {
+      return joke.setupp;
+    },
+    sayPunchLine: function () {
+      return joke.punchLine;
+    },
+  };
+}
