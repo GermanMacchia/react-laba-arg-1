c0a246469d6d016de760c123f1d67b842b54d2b4
Federico Benitez <fedebenitez1696@gmail.com>
2022-08-15 20:15:16 -0300

    exercise 6

--- homeworks/federico-benitez/07-js-basics-2/index.js
+++ homeworks/federico-benitez/07-js-basics-2/index.js
@@ -62,6 +62,12 @@ function solve(arr) {
  * https://www.codewars.com/kata/566044325f8fddc1c000002c
  */
 
+function evenChars(string) {
+  if (string.length < 2 || string.length > 100) return 'invalid string';
+
+  return string.split('').filter((_, i) => i % 2);
+}
+
 /**
  * Exercise 7
  * https://www.codewars.com/kata/545a4c5a61aa4c6916000755
