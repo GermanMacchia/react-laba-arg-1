c989166a416f5471f82725d816786047b8a0ad89
AbrahamTrenyan <77699908+AbrahamTrenyan@users.noreply.github.com>
2022-08-16 20:48:48 -0300

    add shift key functionality

--- homeworks/abraham-trenyan/10-js-dom/script.js
+++ homeworks/abraham-trenyan/10-js-dom/script.js
@@ -5,7 +5,7 @@ for (let i = 0; i <= cols; i++) {
   body.innerHTML += `<div class="column" id=column${i}></div>`;
   for (let j = 0; j <= rows; j++) {
     let col = document.querySelector(`#column${i}`);
-    col.innerHTML += `<div class="cell"><span class="text text--hidden">X: ${i} Y: ${j}</span></div>`;
+    col.innerHTML += `<div class="cell" col=${i} row=${j}><span class="text text--hidden" col=${i} row=${j}>X: ${i} Y: ${j}</span></div>`;
   }
 }
 let column = document.querySelectorAll('.column');
@@ -15,12 +15,33 @@ column.forEach((col) => {
 function select(e) {
   let selected = e.target;
   let text = e.target.children;
-  if (selected.className.includes('cell')) {
-    selected.classList.toggle('cell--colored');
+  let r = selected.getAttribute('row');
+  let c = selected.getAttribute('col');    
+  let activeCol = document.querySelectorAll(`[col="${c}"]`);
+  let activeRow = document.querySelectorAll(`[row="${r}"]`);
+  if (!e.shiftKey) {
+    document.querySelectorAll('.colored').forEach((cell) => {
+      cell.classList.remove('colored')
+      cell.classList.add('text--hidden') // reset text
+    });
+  }
+  else if (selected.className.includes('cell')){
+    activeCol.forEach(cell => {
+      cell.classList.toggle('active')
+    });
+    activeRow.forEach(cell => {
+      cell.classList.toggle('active')
+    });
+    selected.classList.toggle('colored');
     text[0].classList.toggle('text--hidden');
-  } else if (selected.className.includes('text')) {
-    selected.parentElement.classList.toggle('cell--colored');
+  } else if (selected.className.includes('text')) {// Does the same when user clicks on the span instead of the div.
+    activeCol.forEach(cell => {
+      cell.classList.toggle('active')
+    });
+    activeRow.forEach(cell => {
+      cell.classList.toggle('active')
+    });
+    selected.parentElement.classList.toggle('colored');
     selected.classList.toggle('text--hidden');
   }
-  
 }
