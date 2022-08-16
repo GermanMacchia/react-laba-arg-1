9d31b75fde28d28688386b9044bf89554f5ae9af
JulianOvie <ocotemc@gmail.com>
2022-08-16 00:35:14 -0300

    feat: Binary search engine & refact time-elapsed

--- homeworks/julian-oviedo/12-js-data-structures-and-algorithms/script.js
+++ homeworks/julian-oviedo/12-js-data-structures-and-algorithms/script.js
@@ -1,12 +1,12 @@
 import { data } from './MOCK_DATA.js';
+
+//  LINEAR SEARCH
+
 const buttonLinear = document.querySelector('.linear--button');
 const inputLinear = document.querySelector('.linear--input');
 const outputLinear = document.querySelector('.linear--output');
 
-//  LINEAR SEARCH
-
 function clickHandlerLinear() {
-  const start = performance.now();
   for (let i = 0; i < data.length; i++) {
     if (data[i].sku === inputLinear.value) {
       outputLinear.innerText = `Name: ${data[i].name}
@@ -14,8 +14,40 @@ function clickHandlerLinear() {
       Price: ${data[i].price}`;
     }
   }
-  const end = performance.now();
-  const timeTaken = end - start;
-  console.log(timeTaken);
 }
-buttonLinear.addEventListener('click', clickHandlerLinear);
+
+buttonLinear.addEventListener('click', () => {
+  console.time('Time elapsed(Linear)');
+  buttonLinear.addEventListener('click', clickHandlerLinear);
+  console.timeEnd('Time elapsed(Linear)');
+});
+
+// BINARY SEARCH
+
+const dataSort = data.sort((a, b) => (a.sku > b.sku ? 1 : -1));
+const buttonBinary = document.querySelector('.binary--button');
+const inputBinary = document.querySelector('.binary--input');
+const outputBinary = document.querySelector('.binary--output');
+
+function clickHandlerBinary() {
+  let start = 0;
+  let end = dataSort.length - 1;
+  while (start <= end) {
+    let middle = Math.floor((start + end) / 2);
+    if (dataSort[middle].sku == inputBinary.value) {
+      return (outputBinary.innerText = `Name: ${dataSort[middle].name}
+        Pack: ${dataSort[middle].pack}
+        Price: ${dataSort[middle].price}`);
+    } else if (inputBinary.value < dataSort[middle].sku) {
+      end = middle - 1;
+    } else {
+      start = middle + 1;
+    }
+  }
+}
+
+buttonBinary.addEventListener('click', () => {
+  console.time('Time elapsed (Binary)');
+  buttonBinary.addEventListener('click', clickHandlerBinary);
+  console.timeEnd('Time elapsed (Binary)');
+});
