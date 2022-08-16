e1dc5c801e1756f89be7df4b84420cf9c254e7f3
Nicolás <nicolaseugroberto@gmail.com>
2022-08-16 02:42:17 -0300

    Phone number task

--- homeworks/nicolas-roberto/11-js-advanced-3/codewars.js
+++ homeworks/nicolas-roberto/11-js-advanced-3/codewars.js
@@ -47,3 +47,37 @@ const validateMessage = (msg) => {
     sayPunchLine
   }
 } */
+
+// Task 6 - Digit or not
+
+let startsWithLetter = 'String starts with a letter!';
+let startsWithDigit = 'String starts with a digit!';
+
+function testString(string) {
+  //The function takes a string as a parameter and checks if it starts with a digit
+  if (/^\d/.test(string)) {
+    //Depending in the outcome, it returns the variables declared above
+    return startsWithDigit;
+  } else {
+    return startsWithLetter;
+  }
+}
+// Tests
+console.log(testString('Does this string start with a letter?')); // Expected: 'String starts with a letter!'
+console.log(testString('2Does this string start with a digit?')); // Expected: 'String starts with a digit!'
+
+// Task 7 - Argentinian phone nomber validator
+
+function argentinianPhoneNumber(phoneNumber) {
+  if (/^[+]*54[\s-]*(\d{3}[\s-]*\d{4}[\s-]*\d{3})$/.test(phoneNumber)) {
+    return 'This phone number is from Argentina';
+  } else {
+    return 'This phone number is not from Argentina';
+  }
+}
+
+console.log(argentinianPhoneNumber('+54 116-2589-612'));
+console.log(argentinianPhoneNumber('+541162589612'));
+console.log(argentinianPhoneNumber('541162589612'));
+console.log(argentinianPhoneNumber('+380-611-055-561'));
+// /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/
