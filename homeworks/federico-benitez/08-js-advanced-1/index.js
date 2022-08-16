5fb20a3d5b0b7e0a32b05e23e37eb9d9828900f9
Federico Benitez <fedebenitez1696@gmail.com>
2022-08-16 06:53:05 -0300

    exercise 3

--- homeworks/federico-benitez/08-js-advanced-1/index.js
+++ homeworks/federico-benitez/08-js-advanced-1/index.js
@@ -1,7 +1,4 @@
-/**
- * https://github.com/qaprosoft/react-laba-arg-1/blob/main/lectures/08-js-advanced-1/task.md
- */
-//Pluck
+//1. Pluck
 function solution1() {
   const user = {
     username: 'testuser1',
@@ -48,7 +45,7 @@ function solution1() {
   console.log(pluck(nullValue, 'unknown.key')); // null
 }
 
-//Deep clone
+//2. Deep clone
 function solution2() {
   const user = {
     username: 'testuser1',
@@ -69,3 +66,46 @@ function solution2() {
 
   console.log(user.preferences.sound.maxValue === clonedUser.preferences.sound.maxValue); // false
 }
+
+//3. A long time ago
+function offset(arrayDate) {
+  const [givenDate, _] = arrayDate;
+  const current = new Date();
+  const diffMilliseconds = current - givenDate;
+
+  const diffDays = Math.ceil(diffMilliseconds / (1000 * 60 * 60 * 24));
+
+  let seconds = Math.floor(diffMilliseconds / 1000);
+  let minutes = Math.floor(seconds / 60);
+  let hours = Math.floor(minutes / 60);
+
+  if (seconds < 60) {
+    return `${seconds} seconds ago`;
+  }
+
+  if (minutes < 60) {
+    return `${minutes} minutes ago`;
+  }
+  if (hours < 24) {
+    return `${hours} hour${hours > 1 ? 's' : ''} ago`;
+  }
+
+  return `${diffDays} day${diffDays > 1 ? 's' : ''} ago`;
+}
+
+function moment(date, format) {
+  const [days, month, year] = date.split(' ')[0].split('/');
+
+  if (date.split(' ').length > 1) {
+    const hours = date.split(' ')[1];
+    const givenDate = new Date(`${year}-${month}-${days}T${hours}`);
+
+    return [givenDate, format];
+  } else {
+    return new Date(`${year}-${month}-${days}`);
+  }
+}
+
+console.log(offset(moment('16/08/2021 06:35:30', 'DD/MM/YYYY hh:mm:ss')));
+
+//4.Random dates
