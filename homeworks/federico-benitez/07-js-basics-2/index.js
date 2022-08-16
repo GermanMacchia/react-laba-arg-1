f097a3c101ed0c971c5374d65a3b61d853ca80a6
Federico Benitez <fedebenitez1696@gmail.com>
2022-08-15 22:27:55 -0300

    exercise 9

--- homeworks/federico-benitez/07-js-basics-2/index.js
+++ homeworks/federico-benitez/07-js-basics-2/index.js
@@ -92,6 +92,14 @@ const binaryArrayToNumber = (arr) => {
  * https://www.codewars.com/kata/585d7d5adb20cf33cb000235
  */
 
+function findUniq(arr) {
+  arr = arr.sort();
+  if (arr[0] === arr[1]) {
+    return arr[arr.length - 1];
+  } else {
+    return arr[0];
+  }
+}
 /**
  * Exercise 10
  * https://www.codewars.com/kata/581e014b55f2c52bb00000f8
