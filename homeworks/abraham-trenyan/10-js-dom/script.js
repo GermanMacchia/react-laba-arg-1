6d5f0b8466888ae421dd1a6e7039858c17045d0a
AbrahamTrenyan <77699908+AbrahamTrenyan@users.noreply.github.com>
2022-08-17 11:56:27 -0300

    refactor code

--- homeworks/abraham-trenyan/10-js-dom/script.js
+++ homeworks/abraham-trenyan/10-js-dom/script.js
@@ -1,13 +1,14 @@
-const cols = 20;
-const rows = 30;
-const body = document.querySelector('body');
-for (let i = 0; i <= cols; i++) {
-  body.innerHTML += `<div class="column" id=column${i}></div>`;
-  for (let j = 0; j <= rows; j++) {
-    let col = document.querySelector(`#column${i}`);
-    col.innerHTML += `<div class="cell" col=${i} row=${j}><span class="text text--hidden" col=${i} row=${j}>X: ${i} Y: ${j}</span></div>`;
+function createGrid(cols, rows) {
+  const body = document.querySelector('body');
+  for (let i = 0; i <= cols; i++) {
+    body.innerHTML += `<div class="column" id=column${i}></div>`;
+    for (let j = 0; j <= rows; j++) {
+      let col = document.querySelector(`#column${i}`);
+      col.innerHTML += `<div class="cell" col=${i} row=${j}><span class="text text--hidden" col=${i} row=${j}>X: ${i} Y: ${j}</span></div>`;
+    }
   }
 }
+createGrid(20, 30);
 let column = document.querySelectorAll('.column');
 column.forEach((col) => {
   col.addEventListener('click', select);
@@ -16,32 +17,29 @@ function select(e) {
   let selected = e.target;
   let text = e.target.children;
   let r = selected.getAttribute('row');
-  let c = selected.getAttribute('col');    
+  let c = selected.getAttribute('col');
   let activeCol = document.querySelectorAll(`[col="${c}"]`);
   let activeRow = document.querySelectorAll(`[row="${r}"]`);
-  if (!e.shiftKey) {
-    document.querySelectorAll('.colored').forEach((cell) => {
-      cell.classList.remove('colored')
-      cell.classList.add('text--hidden') // reset text
-    });
+  if (e.shiftKey) {
+    selected.className.includes('text') ? selected.parentElement.classList.toggle('colored') : selected.classList.toggle('colored');
+    selected.className.includes('text') ? selected.classList.toggle('text--hidden') : text[0].classList.toggle('text--hidden');
   }
-  else if (selected.className.includes('cell')){
-    activeCol.forEach(cell => {
-      cell.classList.toggle('active')
+  if ((selected.className.includes('cell') || selected.className.includes('text'))) {
+    activeCol.forEach((cell) => {
+      cell.classList.toggle('active');
     });
-    activeRow.forEach(cell => {
-      cell.classList.toggle('active')
+    activeRow.forEach((cell) => {
+      cell.classList.toggle('active');
     });
-    selected.classList.toggle('colored');
-    text[0].classList.toggle('text--hidden');
-  } else if (selected.className.includes('text')) {// Does the same when user clicks on the span instead of the div.
-    activeCol.forEach(cell => {
-      cell.classList.toggle('active')
+    selected.className.includes('text') ? selected.parentElement.classList.toggle('colored') : selected.classList.toggle('colored');
+    selected.className.includes('text') ? selected.classList.toggle('text--hidden') : text[0].classList.toggle('text--hidden');
+  }
+  else if(selected.className.includes('cell') || selected.className.includes('text')){
+    document.querySelectorAll('div').forEach(element => {
+      element.className='active';
+      element.classList.toggle('text--hidden');
     });
-    activeRow.forEach(cell => {
-      cell.classList.toggle('active')
-    });
-    selected.parentElement.classList.toggle('colored');
-    selected.classList.toggle('text--hidden');
+    selected.classList.toggle('selected')
+    selected.classList.toggle()
   }
 }
