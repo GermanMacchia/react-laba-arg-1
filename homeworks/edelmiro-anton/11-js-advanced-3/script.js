09b9325f7ce52cf8e87f2d0ac64436b3d107c244
Edelmiro Anton <anton.edelmiro@gmail.com>
2022-08-17 20:47:14 -0300

    add task 2

--- homeworks/edelmiro-anton/11-js-advanced-3/script.js
+++ homeworks/edelmiro-anton/11-js-advanced-3/script.js
@@ -72,3 +72,27 @@ fetch(url)
     }
   })
   .catch((err) => console.log(err));
+
+//Task 2 - https://www.codewars.com/kata/5a353a478f27f244a1000076/train/javascript
+
+async function sayJoke(apiUrl, jokeId) {
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+
+  if (!data.hasOwnProperty('jokes')) {
+    throw new Error(`No jokes at url: ${apiUrl}`);
+  }
+
+  const joke = data.jokes.find(function (joke) {
+    return joke.id === jokeId;
+  });
+
+  if (!joke) {
+    throw new Error(`No jokes found id: ${jokeId}`);
+  }
+
+  return {
+    saySetup: () => joke.setup,
+    sayPunchLine: () => joke.punchLine,
+  };
+}
