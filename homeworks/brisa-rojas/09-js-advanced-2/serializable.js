3981f7f3c435d8443f1989eb01af5f38df0cfb9c
Brisa Rojas <brojas@itba.edu.ar>
2022-08-18 14:40:44 -0300

    fixed classes comparison

--- homeworks/brisa-rojas/09-js-advanced-2/serializable.js
+++ homeworks/brisa-rojas/09-js-advanced-2/serializable.js
@@ -2,7 +2,6 @@
 const DIVIDER = '---';
 
 class Serializable {
-
   serialize() {
     this.format2Serialize();
     const constructor = this.constructor.name;
@@ -15,35 +14,33 @@ class Serializable {
 
   wakeFrom(serial) {
     let [resurrected, serializedConstructor] = serial.split(DIVIDER);
-    
 
-    if (!this instanceof eval(serializedConstructor)) {
-      throw new Error( `Serialized object is not an instance of ${this.constructor.name}}. Cannot be woken up.`);
+    if ((this.constructor.name).toString() != serializedConstructor) {
+      throw new Error(`Serialized object is not an instance of ${this.constructor.name}. Cannot be woken up.`);
     }
-    
+
     resurrected = Serializable.format2DSerialize(resurrected);
     resurrected = new this.constructor(resurrected);
 
     return resurrected;
   }
 
-  format2Serialize(){ 
+  format2Serialize() {
     for (let key in this) {
       let value = this[key];
 
       if (value instanceof Date) {
         this[key] = { dateTime: value.toISOString(), isDate: true };
       }
-    
+
       if (value === Infinity || value === -Infinity) {
         //if it's an infinity => convert it to string
         this[key] = value.toString();
       }
-      
     }
   }
 
-   static format2DSerialize(serial){
+  static format2DSerialize(serial) {
     let auxiliarObject = JSON.parse(serial);
 
     for (let key in auxiliarObject) {
@@ -55,7 +52,7 @@ class Serializable {
         auxiliarObject[key] = parseFloat(auxiliarObject[key]);
       }
     }
-    
+
     return auxiliarObject;
   }
 }
@@ -85,13 +82,12 @@ let tolik = new UserDTO({
 tolik.printInfo(); //A. Nashovich - 2020327, 1999-01-02T00:00:00.000Z
 
 const serialized = tolik.serialize();
-console.log("This is tolik serial: " + serialized);
+console.log('This is tolik serial: ' + serialized);
 console.log("Let's wake up tolik\n");
 const resurrectedTolik = new UserDTO({}).wakeFrom(serialized);
 
-console.log(resurrectedTolik instanceof UserDTO); // true 
+console.log(resurrectedTolik instanceof UserDTO); // true
 resurrectedTolik.printInfo(); //A. Nashovich - 2020327, 1999-01-02T00:00:00.000Z
-
 
 class Post extends Serializable {
   constructor({ content, date, author }) {
@@ -103,11 +99,20 @@ class Post extends Serializable {
   }
 }
 
-let post = new Post({ content: "How to serialize things?", date: new Date('2000-03-17'), author: "Brisa" });
+let post = new Post({ content: 'How to serialize things?', date: new Date('2000-03-17'), author: 'Brisa' });
 let postSerial = post.serialize();
 console.log(postSerial);
-post = null; 
-let resurrectedPost = new Post({}).wakeFrom(postSerial); 
+post = null;
+let resurrectedPost = new Post({}).wakeFrom(postSerial);
 console.log(resurrectedPost);
 console.log(resurrectedPost instanceof Post); // true
 
+console.log("\nTrying to resurrect something that is not a post\n");
+console.log(serialized.split(DIVIDER)[1]);
+let resurrectedNotPost = new Post({});
+
+try {
+  resurrectedNotPost = resurrectedNotPost.wakeFrom(serialized); //this will throw an error
+} catch (error) {
+  console.log(error);
+}
