be144194ee5ebb8e4c17d9e099c37a405b4ebee6
Edelmiro Anton <anton.edelmiro@gmail.com>
2022-08-19 14:24:05 -0300

    fixed approach of selecting cells

--- homeworks/edelmiro-anton/10-js-dom/script.js
+++ homeworks/edelmiro-anton/10-js-dom/script.js
@@ -1,66 +1,55 @@
 const container = document.querySelector('#container');
+const columns = 20;
+const rows = 30;
+const totalCells = columns * rows;
 
-let columnArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
-let rowArr = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
-];
+for (let i = 1; i <= 30; i++) {
+  for (let j = 1; j <= 20; j++) {
+    const cell = document.createElement('div');
+    cell.classList.add('item');
 
-const columnLength = columnArr.length;
-const rowLength = rowArr.length;
-const totalCells = columnLength * rowLength;
+    //Set atributes for ALL COLUMNS & ROWS
+    cell.setAttribute('x', j);
+    cell.setAttribute('y', i);
 
-function creatPairs(arr1, arr2) {
-  return arr1.reduce((acc, item) => acc.concat(arr2.map((el) => [item, el])), []);
-}
+    const text = document.createElement('p');
+    cell.appendChild(text);
+    container.appendChild(cell);
 
-let pairs = creatPairs(rowArr, columnArr); // output => [Y, X] where Y = row and X = Column
+    cell.addEventListener('click', (e) => {
+      const boxes = document.querySelectorAll('div');
+      const boxText = document.querySelectorAll('p');
+      const columnX = e.currentTarget.getAttribute('x');
+      const rowY = e.currentTarget.getAttribute('y');
+      text.classList.toggle('textOn');
 
-for (let i = 0; i < totalCells; i++) {
-  const cell = document.createElement('div');
-  cell.classList.add('item');
+      //If the user do not hold the shift button, only the clicked cell will be selected
+      // and the previous ones will have its original status
+      if (!e.shiftKey) {
+        boxes.forEach((div) => {
+          div.style.backgroundColor = 'antiquewhite';
+        });
 
-  const text = document.createElement('p');
-  cell.appendChild(text);
-  container.appendChild(cell);
-
-  //Set atributes for ALL COLUMNS & ROWS
-  cell.setAttribute('X', `${pairs[i][1]}`);
-  cell.setAttribute('Y', `${pairs[i][0]}`);
-
-  cell.addEventListener('click', (e) => {
-    // console.log(e.shiftKey);
-
-    const boxes = document.querySelectorAll('div');
-    const boxText = document.querySelectorAll('p');
-    // console.log(e.currentTarget);
-    const columnX = e.currentTarget.getAttribute('x');
-    const rowY = e.currentTarget.getAttribute('y');
-    text.classList.toggle('textOn');
-
-    if (!e.shiftKey) {
-      boxes.forEach((div) => {
-        div.style.backgroundColor = 'antiquewhite';
-      });
-
-      boxText.forEach((text) => {
-        text.innerText = '';
-      });
-    }
-
-    boxes.forEach((box) => {
-      if (box.getAttribute('x') === columnX) {
-        box.style.backgroundColor = 'lightblue';
+        boxText.forEach((text) => {
+          text.innerText = '';
+        });
       }
-      if (box.getAttribute('y') === rowY) {
-        box.style.backgroundColor = 'lightblue';
-      }
+
+      boxes.forEach((box) => {
+        if (box.getAttribute('x') === columnX) {
+          box.style.backgroundColor = 'lightblue';
+        }
+        if (box.getAttribute('y') === rowY) {
+          box.style.backgroundColor = 'lightblue';
+        }
+      });
+      text.innerHTML = `X:${columnX} <br> Y:${rowY}`;
     });
-    text.innerHTML = `X:${pairs[i][1]} <br> Y:${pairs[i][0]}`;
-  });
 
-  cell.addEventListener('click', () => {
-    cell.style.backgroundColor = 'blue';
-    text.style.color = 'white';
-    text.style.textAlign = 'center';
-  });
+    cell.addEventListener('click', () => {
+      cell.style.backgroundColor = 'blue';
+      text.style.color = 'white';
+      text.style.textAlign = 'center';
+    });
+  }
 }
