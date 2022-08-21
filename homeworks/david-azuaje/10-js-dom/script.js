e3d274e6928f28c900a34442737f7a25af1f91af
dazuaje <davidazuaje1084@gmail.com>
2022-08-21 20:53:14 -0300

    HighlightAxis and shift functionality

--- homeworks/david-azuaje/10-js-dom/script.js
+++ homeworks/david-azuaje/10-js-dom/script.js
@@ -1,13 +1,15 @@
 const content = document.querySelector('.content');
-document.addEventListener("DOMContentLoaded", () => {
-    const columns = 20;
-    const rows = 30;
-    for (let i = 0; i < rows * columns; i += 1) {
-        const block = document.createElement('div');
-        block.classList.add('block');
-        content.appendChild(block);
-    }
-});
+
+const columns = 20;
+const rows = 30;
+for (let i = 0; i < rows * columns; i++) {
+    const block = document.createElement('div');
+    block.classList.add('block');
+    content.appendChild(block);
+}
+
+let blocks = document.querySelectorAll('.content .block');
+
 
 function getX(offsetLeft) {
     const getCellX = ((offsetLeft - content.offsetLeft) / 40) + 1;
@@ -20,10 +22,37 @@ function getY(offsetTop) {
     return getCellY;
 }
 
+function highlightLinesXandY(x, y) {
+    [...blocks].map((block) => {
+        if (getX(block.offsetLeft) === x || getY(block.offsetTop) === y) {
+            paintXandYAxis(block);
+        }
+    });
+}
+
+function paintXandYAxis(block) {
+    block.classList.add('block-lines');
+}
+
 content.addEventListener("click", (e) => {
-    const { offsetLeft, offsetTop } = e.target;
-    const x = getX(offsetLeft);
-    const y = getY(offsetTop)
-    e.target.innerHTML = `X: ${x}  Y: ${y}`;
-    e.target.classList.toggle('activeCell')
+    if (!e.shiftKey) {
+        clearCellsAxis();
+    };
+
+
+    const x = getX(e.target.offsetLeft);
+    const y = getY(e.target.offsetTop);
+    highlightLinesXandY(x, y);
+    e.target.innerHTML = `X:${x}  Y:${y}`;
+    e.target.classList.add('block-selected')
+
 });
+
+
+function clearCellsAxis() {
+    blocks.forEach((block) => {
+        block.classList.remove('block-selected');
+        block.innerText = '';
+        block.classList.remove('block-lines');
+    });
+}
