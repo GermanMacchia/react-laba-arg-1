dd5304cfd61c5866205934a395c28bb032458a4c
Brisa Rojas <brojas@itba.edu.ar>
2022-09-02 20:58:31 -0300

    Adds function component for lights

--- homeworks/brisa-rojas/16-react-basics/script.js
+++ homeworks/brisa-rojas/16-react-basics/script.js
@@ -13,16 +13,27 @@ class TrafficLight extends React.Component {
     let currentColor = this.state.color;
     return (
       <div className="traffic-light">
-        <div className="traffic-light__upper-thingy"></div>
-        <div className="traffic-light__lights-container">
-          <div className="traffic-light__light traffic-light__light_red"></div>
-          <div className="traffic-light__light traffic-light__light_yellow"></div>
-          <div className="traffic-light__light traffic-light__light_green"></div>
+        <div className="traffic-light__head"></div>
+        <div className="traffic-light__body">
+            {Light('red', currentColor)}
+            {Light('yellow', currentColor)}
+            {Light('green', currentColor)}
         </div>
       </div>
     );
   }
 
+}
+
+function Light(color, activeLight) {
+    let className = "traffic-light__light";
+    if (color === activeLight) {
+        className += " traffic-light__light_" + color;
+    }
+    
+    return (
+        <div className={className} key={color}/>
+    )
 }
 
 root.render(<TrafficLight />);
