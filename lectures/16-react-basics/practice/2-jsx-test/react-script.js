bde2d7f8acafe4c6da32e15fe8616135fd7a99cf
Mykhailo Melnyk <mmelnyk@solvd.com>
2022-09-02 19:14:09 +0300

    Add 16 lecture

--- lectures/16-react-basics/practice/2-jsx-test/react-script.js
+++ lectures/16-react-basics/practice/2-jsx-test/react-script.js
@@ -3,6 +3,7 @@ const Heading = () => {
     const name = "Misha";
     return <h1 id="test-id">Hello from jsx {name}</h1>
 };
+
 const domContainer = document.querySelector('#root_container');
 const root = ReactDOM.createRoot(domContainer);
 root.render(<Heading />);
