c3170e94fa5707bdd3df8d27acf974b8c8718492
Brisa Rojas <brojas@itba.edu.ar>
2022-09-02 18:56:15 -0300

    Adds static UI for traffic light

---
+++ homeworks/brisa-rojas/16-react-basics/script.js
@@ -0,0 +1,28 @@
+const rootEl = document.querySelector('#root');
+const root = ReactDOM.createRoot(rootEl);
+
+class TrafficLight extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: 'red',
+    };
+  }
+
+  render() {
+    let currentColor = this.state.color;
+    return (
+      <div className="traffic-light">
+        <div className="traffic-light__upper-thingy"></div>
+        <div className="traffic-light__lights-container">
+          <div className="traffic-light__light traffic-light__light--red"></div>
+          <div className="traffic-light__light traffic-light__light--yellow"></div>
+          <div className="traffic-light__light traffic-light__light--green"></div>
+        </div>
+      </div>
+    );
+  }
+
+}
+
+root.render(<TrafficLight />);
