d99af710df19259faeddcf0b4cff0256c97d9309
German O. Macchia <germanmacchia@gmail.com>
2022-09-03 15:53:56 -0300

    first part task

--- homeworks/german-macchia/16-react-basics/script.js
+++ homeworks/german-macchia/16-react-basics/script.js
@@ -1,54 +1,64 @@
-// const element = React.createElement('h1', {id: 'element-id', children: "Heloo guys!"});
-const TrafficLight = () => {
-    let time = 1000
-    return (
-        <div className="traffic-light">   
-            <div className="container">
-                <Light color="red" time={time * 4}/>
-                <Light color="yellow" time={time * 2}/>
-                <Light color="green" time={time}/>
-            </div> 
-        </div>
 
-    )
+const TrafficLight = () => {
+  let time = 1000;
+  return (
+    <div className="container">
+      <div className="post" />
+      <div className="traffic-light">
+        <div className="traffic-light__light-rail">
+          <Light color="red" time={time * 4} />
+          <Light color="yellow" time={time * 2} />
+          <Light color="green" time={time} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
-class Light extends React.Component{
+class Light extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      light: props.color,
+      stop: "grey",
+      actual: !(props.color === "green") ? "grey" : "green",
+      time: props.time,
+    };
+  }
 
-    constructor(props){
-        super(props)
-        this.state = {
-            light: props.color,
-            stop: "grey",
-            actual: !(props.color === "green")? "grey" : "green",
-            time: props.time
-        }
-    }
+  twinkle = () => {
 
-    twinkle = () =>{
+    setTimeout(() => {
+      this.setState({
+        ...this.state,
+        actual: this.state.light,
+      });
+
+      if (!(this.state.light === "red")) {
         setTimeout(() => {
-            this.setState({
-                ...this.state,
-                actual: this.state.light
-            })
-            setTimeout(() => {
-                this.setState({
-                    ...this.state,
-                    actual: this.state.stop
-                })
-            }, this.state.time); 
-        }, this.state.time);       
-    }  
+          this.setState({
+            ...this.state,
+            actual: this.state.stop,
+          });
+        }, this.state.time);
+      }
+    }, this.state.time);
+  };
 
-    componentDidMount(){
-        this.twinkle()
-    }
+  componentDidMount() {
+    this.twinkle();
+  }
 
-    render(){
-        return <div className="light" style={{backgroundColor: this.state.actual}}></div>
-    }
+  render() {
+    return (
+      <div
+        className="light"
+        style={{ backgroundColor: this.state.actual }}
+      ></div>
+    );
+  }
 }
 
-const domContainer = document.querySelector('#root');
+const domContainer = document.querySelector("#root");
 const root = ReactDOM.createRoot(domContainer);
 root.render(<TrafficLight />);
