0ca2d9a762e8d1b98c5f8d135901b74f6134bcbc
Brisa Rojas <brojas@itba.edu.ar>
2022-09-02 21:45:58 -0300

    Adds state change to re-render

--- homeworks/brisa-rojas/16-react-basics/script.js
+++ homeworks/brisa-rojas/16-react-basics/script.js
@@ -1,45 +1,40 @@
 const rootEl = document.querySelector('#root');
 const root = ReactDOM.createRoot(rootEl);
+const TIME_LAPSE = 1500;
 
 class TrafficLight extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       lightColors: ['red', 'yellow', 'green'],
-      counter:0
+      counter: 0,
     };
   }
 
-    // componentDidMount() {
-    //     setInterval()
-    // }
   render() {
     let lightIndex = this.state.counter % 3;
     let currentColor = this.state.lightColors[lightIndex];
-    this.setState(()=> {counter: this.state.counter + 1});
+    setInterval(() => {this.setState({lightColors: ['red', 'yellow', 'green'] , counter: lightIndex + 1})} , TIME_LAPSE);
     return (
       <div className="traffic-light">
         <div className="traffic-light__head"></div>
         <div className="traffic-light__body">
-            {Light('red', currentColor)}
-            {Light('yellow', currentColor)}
-            {Light('green', currentColor)} 
+          {Light('red', currentColor)}
+          {Light('yellow', currentColor)}
+          {Light('green', currentColor)}
         </div>
       </div>
     );
   }
-
 }
 
 function Light(color, activeLight) {
-    let className = "traffic-light__light";
-    if (color === activeLight) {
-        className += " traffic-light__light_" + color;
-    }
-    
-    return (
-        <div className={className} key={color}/>
-    )
+  let className = 'traffic-light__light';
+  if (color === activeLight) {
+    className += ' traffic-light__light_' + color;
+  }
+
+  return <div className={className} key={color} />;
 }
 
 root.render(<TrafficLight />);
