20000a1ceb810f0894e024fade8ac8c02a01ea06
AbrahamTrenyan <77699908+AbrahamTrenyan@users.noreply.github.com>
2022-09-05 15:27:53 -0300

    create traffic lights layout

--- homeworks/abraham-trenyan/16-react-basics/script.js
+++ homeworks/abraham-trenyan/16-react-basics/script.js
@@ -1,6 +1,7 @@
-const Element = () => {
+/* const Element = () => {
     return <h1>Hello World</h1>;
 };
 const body = document.querySelector('.root');
 const root = ReactDOM.createRoot(body);
 root.render(<Element />);
+ */
