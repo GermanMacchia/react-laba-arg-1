6ebf3dc373c4fe88039f521f46ad16a35d10f36d
Emiliano Acerbi <eacerbip@gmail.com>
2022-09-07 11:07:55 -0300

    working correctly

--- homeworks/emiliano-acerbi/17-class-and-functional-components/src/App.jsx
+++ homeworks/emiliano-acerbi/17-class-and-functional-components/src/App.jsx
@@ -1,7 +1,6 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 
 const URL = 'https://tinyfac.es/api/data?limit=1&quality=0';
-
 class App extends Component {
   constructor(props) {
     super(props);
@@ -10,15 +9,17 @@ class App extends Component {
     };
 
     this.fetchSingleUser = this.fetchSingleUser.bind(this);
+    this.refreshUser = this.refreshUser.bind(this);
+    this.refreshAllUsers = this.refreshAllUsers.bind(this);
   }
 
   async fetchSingleUser() {
     try {
       let response = await fetch(URL);
       if (response.status === 200) {
-        let data = await response.json();
+        let fetchedUsers = await response.json();
         this.setState({
-          users: [...this.state.users, data[0]],
+          users: [...this.state.users, fetchedUsers[0]],
         });
       } else {
         throw 'Error fetching users list';
@@ -28,29 +29,53 @@ class App extends Component {
     }
   }
 
+  async refreshUser(index) {
+    let response = await fetch(URL);
+    let fetchedUsers = await response.json();
+    let newUsers = { users: [...this.state.users] };
+    newUsers.users[index] = fetchedUsers[0];
+    this.setState(newUsers);
+  }
+
+  async refreshAllUsers() {
+    let usersLength = this.state.users.length;
+    let response = await fetch(`https://tinyfac.es/api/data?limit=${usersLength}&quality=0`);
+    let fetchedUsers = await response.json();
+    this.setState({
+      users: fetchedUsers,
+    });
+  }
+
   render() {
-    console.log(this.state);
+    // If there are no users, the refresh all button will be disabled
+    const isButtonDisabled = this.state.users.length === 0 ? true : false;
 
     return (
-      <>
-        <main className="container">
+      <div className="container">
+        <main className="main">
           {/* Add users */}
           <div className="empty-user" onClick={this.fetchSingleUser}>
             <div className="plus-sign-hor"></div>
             <div className="plus-sign-ver"></div>
           </div>
-          {this.state.users.map((user) => {
+
+          {/* Iterates over all the users available */}
+          {this.state.users.map((user, index) => {
             return (
-              <div className="image" key={user.id}>
+              <div className="image" key={user.id} onClick={() => this.refreshUser(index)}>
                 <img src={user.url} width={240} height={240} />
               </div>
             );
           })}
         </main>
-        <footer>
-          <button onClick={this.fetchData}>Refresh All</button>
+
+        {/* Footer */}
+        <footer className="footer">
+          <button disabled={isButtonDisabled} className="refresh-btn" onClick={this.refreshAllUsers}>
+            Refresh All
+          </button>
         </footer>
-      </>
+      </div>
     );
   }
 }
