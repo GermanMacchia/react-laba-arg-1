5d9ec7ca6fcf591b38d7e97ed3f4572d22998cc5
German O. Macchia <germanmacchia@gmail.com>
2022-09-08 11:28:03 -0300

    first approach solution

--- homeworks/german-macchia/17-class-and-functional-components/avatar-app-task/src/App.js
+++ homeworks/german-macchia/17-class-and-functional-components/avatar-app-task/src/App.js
@@ -13,49 +13,68 @@ class App extends React.Component {
     };
   }
 
-  reload = (string) => {
-    console.log(string);
-  };
-
   displayPhotos() {
     return this.state.photos.map((photo) => {
       return (
         <PhotoContainer
-          image={photo.url}
+          url={photo.url}
           name={photo.name}
-          key={`Container for ${photo.name}`}
+          key={photo.id.toString()}
           id={photo.id}
-          setNew={this.reload}
+          reload={this.reloadPhoto}
         />
       );
     });
   }
 
-  getPhoto = () => {
+  getPhotos = async (cant) => {
+    let api = `https://tinyfac.es/api/data?limit=${cant}`;
+    const response = await fetch(api);
+    const json = await response.json();
+    return json.map((photo) => {
+      return {
+        id: photo.id,
+        url: photo.url,
+        name: `${photo.first_name} ${photo.last_name}`,
+      };
+    });
+  };
+
+  pushPhoto = async () => {
+    const cantPhoto = 1;
     this.setState(() => ({ loading: true }));
-    fetch("https://tinyfac.es/api/data?limit=1&quality=1")
-      .then((res) => res.json())
-      .then((json) => {
-        this.setState({
-          photos: [
-            ...this.state.photos,
-            {
-              id: json[0].id,
-              url: json[0].url,
-              name: `${json[0].first_name} ${json[0].last_name}`,
-            },
-          ],
-        });
-        this.setState(() => ({ loading: false }));
-      });
-  }
+    const [photo] = await this.getPhotos(cantPhoto);
+    this.setState((state) => ({
+      photos: [...state.photos, photo],
+    }));
+    this.setState(() => ({ loading: false }));
+  };
+
+  reloadPhoto = async (id) => {
+    const cantPhoto = 1;
+    const [photo] = await this.getPhotos(cantPhoto);
+    const newStatePhotos = [...this.state.photos];
+    const idx = newStatePhotos.findIndex((photo) => photo.id === id);
+    newStatePhotos.splice(idx, 1, {
+      id: photo.id,
+      url: photo.url,
+      name: `${photo.first_name} ${photo.last_name}`,
+    });
+    this.setState(() => ({ photos: newStatePhotos }));
+  };
+
+  refreshAll = async () => {
+    const statePhotosLength = this.state.photos.length;
+    const newPhotos = await this.getPhotos(statePhotosLength);
+    this.setState(() => ({ photos: newPhotos }));
+  };
 
   render() {
     return (
       <div className="App">
         <div className="grid-container">
           {this.displayPhotos()}
-          <button className="square_button" onClick={this.getPhoto}>
+          <button className="square_button" onClick={this.pushPhoto}>
             {this.state.loading ? (
               <img
                 className="square_button__loading"
@@ -72,7 +91,9 @@ class App extends React.Component {
           </button>
         </div>
         <footer>
-          <button className="footer__button">Refresh All</button>
+          <button className="footer__button" onClick={this.refreshAll}>
+            Refresh All
+          </button>
         </footer>
       </div>
     );
--- homeworks/german-macchia/17-class-and-functional-components/avatar-app-task/src/components/PhotoContainer.js
+++ homeworks/german-macchia/17-class-and-functional-components/avatar-app-task/src/components/PhotoContainer.js
@@ -1,53 +1,28 @@
 import React from "react";
-import reloadIcon from "../assets/refresh-reload.svg";
 
-class PhotoContainer extends React.Component {
+class PhotoContainer extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = {
-      loading: true,
-      image: props.image,
+      url: props.url,
       name: props.name,
       id: props.id,
-      setNew: props.setNew,
+      reload: props.reload,
     };
-  }
-
-  reload() {
-    this.setState(() => ({ loading: true }));
-    fetch("https://tinyfac.es/api/data?limit=1&quality=1")
-      .then((res) => res.json())
-      .then((json) => {
-        this.setState({
-          photos: [
-            ...this.state.photos,
-            {
-              id: json[0].id,
-              url: json[0].url,
-              name: `${json[0].first_name} ${json[0].last_name}`,
-            },
-          ],
-        });
-        this.setState(() => ({ loading: false }));
-      });
   }
 
   render() {
     return (
       <div
-        onClick={() => this.state.setNew(`From ${this.state.name}`)}
         className="container"
+        onClick={() => this.state.reload(this.state.id)}
       >
         <img
-          className="container__reload_image"
-          src={reloadIcon}
-          alt="reload"
-        />
-        <img
           className="container__profile_image"
-          src={this.state.image}
+          src={this.state.url}
           alt={this.state.name}
         />
+        <div className="container__image_box" />
       </div>
     );
   }
