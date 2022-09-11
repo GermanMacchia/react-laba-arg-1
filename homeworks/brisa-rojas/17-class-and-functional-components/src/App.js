98056e221257117c29806657fb1c7ccb0304b8b3
Brisa Rojas <brojas@itba.edu.ar>
2022-09-11 12:11:39 -0300

    formatted

--- homeworks/brisa-rojas/17-class-and-functional-components/src/App.js
+++ homeworks/brisa-rojas/17-class-and-functional-components/src/App.js
@@ -7,40 +7,38 @@ class App extends React.Component {
     super(props);
     this.state = {
       avatarCounter: 0,
-      avatarImages : []
-    }; 
+      avatarImages: [],
+    };
     this.addAvatar = this.addAvatar.bind(this);
-    this.getNewAvatarImage = this.getNewAvatarImage.bind(this); 
+    this.getNewAvatarImage = this.getNewAvatarImage.bind(this);
   }
 
   async addAvatar() {
-    const newAvatarImage =  await this.getNewAvatarImage();
-    this.setState(() => { 
-      return ({
+    const newAvatarImage = await this.getNewAvatarImage();
+    this.setState(() => {
+      return {
         avatarCounter: this.state.avatarCounter + 1,
-        avatarImages: [...this.state.avatarImages, newAvatarImage]
-      })
+        avatarImages: [...this.state.avatarImages, newAvatarImage],
+      };
     });
   }
 
   async getNewAvatarImage() {
     return fetch('https://tinyfac.es/api/data?limit=1')
-      .then(response => response.json())
-      .then(data => data[0].url);
+      .then((response) => response.json())
+      .then((data) => data[0].url);
   }
 
   async refreshAvatarImage(avatarId) {
     let newAvatarURL = await this.getNewAvatarImage();
-    let newAvatarImages = this.state.avatarImages.map(
-      (avatarImage, index) => {
-        if (index === avatarId) {
-          return newAvatarURL;
-        } else {
-          return avatarImage;
-        }
+    let newAvatarImages = this.state.avatarImages.map((avatarImage, index) => {
+      if (index === avatarId) {
+        return newAvatarURL;
+      } else {
+        return avatarImage;
       }
-    );
-    this.setState({avatarImages: newAvatarImages});
+    });
+    this.setState({ avatarImages: newAvatarImages });
   }
 
   render() {
@@ -48,25 +46,32 @@ class App extends React.Component {
       <div className="App">
         <div className="avatar-container">
           {this.state.avatarImages.map((avatarImage, index) => {
-            return <AvatarTile  key= {"Avatar #" + index} avatarURL = {avatarImage} onClick={() => this.refreshAvatarImage(index)}/>
-          })} 
-          <AddButton onClick= {this.addAvatar}/>
+            return (
+              <AvatarTile
+                key={'Avatar #' + index}
+                avatarURL={avatarImage}
+                onClick={() => this.refreshAvatarImage(index)}
+              />
+            );
+          })}
+          <AddButton onClick={this.addAvatar} />
         </div>
-        <RefreshAllButton onClick={() => {
-          this.state.avatarImages.forEach((avatarImage, index) => {
-            this.refreshAvatarImage(index);
-          });
-        }}/>
+        <RefreshAllButton
+          onClick={() => {
+            this.state.avatarImages.forEach((avatarImage, index) => {
+              this.refreshAvatarImage(index);
+            });
+          }}
+        />
       </div>
     );
   }
 }
 
-
 class AvatarTile extends React.Component {
   render() {
     return (
-      <div className="avatar-tile tile" onClick={this.props.onClick} >
+      <div className="avatar-tile tile" onClick={this.props.onClick}>
         <img className="avatar-tile__img" src={this.props.avatarURL} alt="avatar img" />
         <div className="avatar-tile__overlay">
           <img className="avatar-tile__refresh-icon" src={refreshIcon} alt="refresh img" />
@@ -82,7 +87,7 @@ class AddButton extends React.Component {
       <div className="add-avatar-tile tile" onClick={this.props.onClick}>
         +
       </div>
-    )
+    );
   }
 }
 
@@ -92,7 +97,7 @@ class RefreshAllButton extends React.Component {
       <div className="refresh-all-button" onClick={this.props.onClick}>
         REFRESH ALL
       </div>
-    )
+    );
   }
 }
 
