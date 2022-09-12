04253a747c370e35f50513c831b3566e140b2761
AbrahamTrenyan <77699908+AbrahamTrenyan@users.noreply.github.com>
2022-09-12 18:34:03 -0300

    change component to functional

--- homeworks/abraham-trenyan/18-hooks/src/Faces/faces.jsx
+++ homeworks/abraham-trenyan/18-hooks/src/Faces/faces.jsx
@@ -1,10 +1,34 @@
 import './faces.css';
+import { useState } from 'react';
 const Faces = () => {
+  const [images, setImages] = useState([]);
+  const getImg = (e, imgAmount) => {
+    fetch(`https://tinyfac.es/api/data?limit=${imgAmount}&quality=0`)
+      .then((res) => res.json())
+      .then((result) => {
+        if (e.target.className.includes('add')) {
+          // If we just need to add one photo, we take the first object from the results array and push its url after the previous URLs that we have
+          setImages([...images, result[0].url]);
+        } else if (e.target.className === 'refresh-all__button') {
+          // In case the request was triggered by the refresh all button,we must replace the old state with an array of new urls(newImgs)
+          let newImgs = [];
+          for (let i = 0; i < result.length; i++) {
+            newImgs.push(result[i].url);
+          }
+          setImages(newImgs);
+        } else if (e.target.className.includes('face')) {
+          // If a face triggered the event, we replace that face
+          let newState = [...images];
+          newState[e.target.parentElement.id] = result[0].url;
+          setImages([...newState]);
+        }
+      });
+  };
   return (
     <main>
       <section className="faces__container">
-        {this.state.images.map((url, index) => (
-          <div className="face" id={index} key={index} onClick={(e) => this.getImg(e, 1)}>
+        {images.map((url, index) => (
+          <div className="face" id={index} key={index} onClick={(e) => getImg(e, 1)}>
             <div className="face__img face__img--layer">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -25,14 +49,14 @@ const Faces = () => {
             <img className="face__img" src={url} loading="lazy" alt="random person"></img>
           </div>
         ))}
-        <div className="add" onClick={(e) => this.getImg(e, 1)}>
+        <div className="add" onClick={(e) => getImg(e, 1)}>
           <div className="add__horizontal-line"></div>
           <div className="add__vertical-line"></div>
         </div>
       </section>
       <footer className="refresh-all">
-        {this.state.images.length > 1 ? (
-          <button className="refresh-all__button" onClick={(e) => this.getImg(e, this.state.images.length)}>
+        {images.length > 1 ? (
+          <button className="refresh-all__button" onClick={(e) => getImg(e, images.length)}>
             Refresh All
           </button>
         ) : (
