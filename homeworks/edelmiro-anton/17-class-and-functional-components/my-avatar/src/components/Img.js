40d6c33e41df5573792da60627c169a0010ee0a5
Edelmiro Anton <anton.edelmiro@gmail.com>
2022-09-12 22:51:09 -0300

    add styles for refresh avatar button

--- homeworks/edelmiro-anton/17-class-and-functional-components/my-avatar/src/components/Img.js
+++ homeworks/edelmiro-anton/17-class-and-functional-components/my-avatar/src/components/Img.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../styles.css';
+import Refresh from '../assets/refresh-button.svg';
 
 export default class Img extends React.Component {
   render() {
     return (
       <>
-        <img key={this.props.key} src={this.props.src} alt="" className="avatar" onClick={this.props.onClick} />
+        <img src={Refresh} className="refresh-button" onClick={this.props.onClick} />
+        <img src={this.props.src} alt="" className="avatar" />
       </>
     );
   }
