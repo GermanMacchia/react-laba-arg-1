ebef8efc1d83fbea057fe58ea37944ab95894b7b
Emiliano Acerbi <eacerbip@gmail.com>
2022-09-16 15:09:10 -0300

    setup

--- homeworks/emiliano-acerbi/20-routing-redux/redux/src/App.jsx
+++ homeworks/emiliano-acerbi/20-routing-redux/redux/src/App.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 function App() {
   return (
-    <main>
-      <h1 className="text-blue-400 text-5xl">Solvd</h1>
+    <main className="min-h-screen bg-gradient font-primary text-3xl grid place-content-center">
+      <div className="flex">
+        <input className="w-[554px] h-[65px] px-5 text-neutral-700 " value="Create Todo-Task" />
+        <button className="bg-add-bg text-white w-[122px] h-[65px]">Add</button>
+      </div>
     </main>
   );
 }
