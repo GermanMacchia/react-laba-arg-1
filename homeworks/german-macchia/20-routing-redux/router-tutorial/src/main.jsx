f5e63cd7afc85b273439fa9d2f6a30f2c0e07c6f
German O. Macchia <germanmacchia@gmail.com>
2022-09-18 23:00:19 -0300

    router tutorial - index routes

--- homeworks/german-macchia/20-routing-redux/router-tutorial/src/main.jsx
+++ homeworks/german-macchia/20-routing-redux/router-tutorial/src/main.jsx
@@ -8,9 +8,9 @@ import Root, {
   action as rootAction,
 } from "./routes/root";
 import Contact, { loader as contactLoader } from "./routes/contact";
-import EditContact, {
-  action as editAction,
-} from "./routes/edit";
+import EditContact, { action as editAction } from "./routes/edit";
+import { action as destroyAction } from "./routes/destroy";
+import Index from "./routes/index";
 
 const router = createBrowserRouter([
   {
@@ -20,6 +20,7 @@ const router = createBrowserRouter([
     loader: rootLoader,
     action: rootAction,
     children: [
+      { index: true, element: <Index /> },
       {
         path: "contacts/:contactId",
         element: <Contact />,
@@ -30,6 +31,11 @@ const router = createBrowserRouter([
         element: <EditContact />,
         loader: contactLoader,
         action: editAction,
+      },
+      {
+        path: "contacts/:contactId/destroy",
+        action: destroyAction,
+        errorElement: <div>Oops! There was an error.</div>,
       },
     ],
   },
