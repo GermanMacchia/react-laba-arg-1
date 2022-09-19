903cad98f9d66a61c107d99b9e01b8929f2eb224
Emiliano Acerbi <eacerbip@gmail.com>
2022-09-19 12:25:04 -0300

    half way through

--- homeworks/emiliano-acerbi/20-routing-redux/routing/src/main.jsx
+++ homeworks/emiliano-acerbi/20-routing-redux/routing/src/main.jsx
@@ -3,19 +3,28 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
 import ErrorPage from './error-page';
 import './index.css';
-
-import Root from './routes/root';
-import Contact from './routes/contact';
+import Root, { loader as rootLoader, action as rootAction } from './routes/root';
+import Contact, { loader as contactLoader } from './routes/contact';
+import EditContact, { action as editAction } from './routes/edit';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
     errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
     children: [
       {
         path: 'contacts/:contactId',
         element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: 'contacts/:contactId/edit',
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction,
       },
     ],
   },
