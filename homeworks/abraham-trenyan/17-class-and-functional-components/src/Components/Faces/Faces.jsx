0d94529abb9fceb1a5ea0dabf62d3ff4498506f5
AbrahamTrenyan <77699908+AbrahamTrenyan@users.noreply.github.com>
2022-09-19 22:20:09 -0300

    refactor: divide request function,one for each component

--- homeworks/abraham-trenyan/17-class-and-functional-components/src/Components/Faces/Faces.jsx
+++ homeworks/abraham-trenyan/17-class-and-functional-components/src/Components/Faces/Faces.jsx
@@ -6,56 +6,62 @@ class Faces extends Component {
     super();
     this.state = { images: [] };
   }
-  async getImg(e, imgAmount) {
-    let res = await fetch(`https://tinyfac.es/api/data?limit=${imgAmount}&quality=0`);
-    res.json().then((result) => {
+  addImg = async () => {
+    let response = await fetch(`https://tinyfac.es/api/data?limit=1&quality=0`);
+    response.json().then((result) => {
       this.setState((prevState) => {
-        if (e.target.className.includes('add')) {
-          return {
-            // If we just need to add one photo, we take the first object from the results array and push its url after the previous URLs that we have
-            images: [...prevState.images, result[0].url],
-          };
-        } else if (e.target.className === 'refresh-all__button') {
-          // In case the request was triggered by the refresh all button,we must replace the old state with an array of new urls(newImgs)
-          let newImgs = [];
-          for (let i = 0; i < result.length; i++) {
-            newImgs.push(result[i].url);
-          }
-          return {
-            images: [...newImgs],
-          };
-        } else if (e.target.className.includes('face')) {
-          // If a face triggered the event, we replace that face
-          let newState = [...prevState.images];
-          newState[e.target.parentElement.id] = result[0].url;
-          return {
-            images: [...newState],
-          };
-        }
+        return {
+          images: [...prevState.images, result[0].url],
+        };
       });
     });
-  }
-
+  };
+  refreshImg = async (e) => {
+    let response = await fetch(`https://tinyfac.es/api/data?limit=1&quality=0`);
+    response.json().then((result) => {
+      this.setState((prevState) => {
+        let newState = [...prevState.images];
+        newState[e.target.parentElement.id] = result[0].url;
+        return {
+          images: [...newState],
+        };
+      });
+    });
+  };
+  refreshAll = async () => {
+    let response = await fetch(`https://tinyfac.es/api/data?limit=${this.state.images.length}&quality=0`);
+    response.json().then((result) => {
+      this.setState(() => {
+        let newImgs = [];
+        for (let i = 0; i < result.length; i++) {
+          newImgs.push(result[i].url);
+        }
+        return {
+          images: [...newImgs],
+        };
+      });
+    });
+  };
   render() {
     return (
       <main>
         <section className="faces__container">
           {this.state.images.map((url, index) => (
-            <div className="face" id={index} key={index} onClick={(e) => this.getImg(e, 1)}>
+            <div className="face" id={index} key={index} onClick={(e) => this.refreshImg(e)}>
               <div className="face__img face__img--layer">
                 <RefreshIcon />
               </div>
               <img className="face__img" src={url} loading="lazy" alt="random person"></img>
             </div>
           ))}
-          <div className="add" onClick={(e) => this.getImg(e, 1)}>
+          <div className="add" onClick={this.addImg}>
             <div className="add__horizontal-line"></div>
             <div className="add__vertical-line"></div>
           </div>
         </section>
         <footer className="refresh-all">
           {this.state.images.length > 1 && (
-            <button className="refresh-all__button" onClick={(e) => this.getImg(e, this.state.images.length)}>
+            <button className="refresh-all__button" onClick={this.refreshAll}>
               Refresh All
             </button>
           )}
