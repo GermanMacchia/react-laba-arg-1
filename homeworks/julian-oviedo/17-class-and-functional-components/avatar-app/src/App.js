5c39eb4d7db8085030435b421599c942026c3688
JulianOvie <julianovie234@gmail.com>
2022-09-20 14:25:08 -0300

    add styles and logic

--- homeworks/julian-oviedo/17-class-and-functional-components/avatar-app/src/App.js
+++ homeworks/julian-oviedo/17-class-and-functional-components/avatar-app/src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
+import React from 'react';
+import refreshImg from './assets/images/refresh.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const URL = 'https://tinyfac.es/api/data?limit=1&quality=0';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      avatars: [],
+    };
+    this.addAvatar = this.addAvatar.bind(this);
+    this.refreshAllAvatars = this.refreshAllAvatars.bind(this);
+    this.refreshClickedAvatar = this.refreshClickedAvatar.bind(this);
+  }
+
+  addAvatar() {
+    fetch(URL, { method: 'GET' }).then((res) => {
+      res.json().then((data) => {
+        this.setState((state) => {
+          const newList = [...state.avatars, data[0].url];
+          return { avatars: newList };
+        });
+      });
+    });
+  }
+
+  refreshAllAvatars() {
+    this.setState({
+      avatars: [],
+    });
+  }
+
+  refreshClickedAvatar(avatarId) {
+    fetch(URL, { method: 'GET' }).then((res) => {
+      res.json().then((data) => {
+        const newAvatar = data[0].url;
+        let newList = this.state.avatars.map((avatar, index) => {
+          if (index === avatarId) {
+            return newAvatar;
+          }
+          return avatar;
+        });
+        this.setState({ avatars: newList });
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="cards-container">
+          {this.state.avatars.map((avatar, index) => {
+            return (
+              <div className="card">
+                <img className="card__avatar" key={avatar} src={avatar} alt=""></img>
+                <img
+                  className="card__refreshIcon"
+                  key="refreshIcon"
+                  src={refreshImg}
+                  onClick={() => this.refreshClickedAvatar(index)}
+                  alt=""
+                ></img>
+              </div>
+            );
+          })}
+          <button className="addButton" onClick={this.addAvatar}>
+            +
+          </button>
+        </div>
+        <div className="refreshButton-container">
+          <button className="refreshButton" onClick={this.refreshAllAvatars}>
+            REFRESH ALL
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
