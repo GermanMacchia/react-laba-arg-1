358521753618cab2a92663e22103a3a67ea05750
AbrahamTrenyan <77699908+AbrahamTrenyan@users.noreply.github.com>
2022-09-19 21:35:44 -0300

    add reset and delete buttons functionalities

--- homeworks/abraham-trenyan/19-react-api/src/App.js
+++ homeworks/abraham-trenyan/19-react-api/src/App.js
@@ -8,14 +8,27 @@ function App() {
   const [buttons, setButtons] = useState(['C', '÷', 'X', 'delete', 7, 8, 9, '-', 4, 5, 6, '+', 1, 2, 3, '%', 0, '.', '=']);
   const [operation, setOperation] = useState([]);
   const handleClick = (btnValue) => {
-    setOperation([...operation, btnValue])
+    if (typeof btnValue === 'number' || (typeof btnValue !== 'number' && typeof operation[operation.length - 1] === 'number')) {
+      setOperation([...operation, btnValue]); //Only sets sign key, if we have a number before it.
+    }
   }
+  const resetCalc = () => {
+    setOperation([]);
+  }
+  const deleteLast = () => {
+    operation.pop();
+    setOperation([...operation]);
+  }
+  const handleEqual = () => {
+    
+  } 
   return (
     <Container>
-      <Screen value={operation}/>
+      <Screen value={operation} />
       <Keyboard>
         {buttons.map((btn, i) => (
-          <Button key={i} value={btn} onClick={() => handleClick(btn)} />
+          <Button key={i} value={btn}
+            onClick={btn === 'C' ? resetCalc : btn === 'delete' ? deleteLast : () => handleClick(btn)} />
         ))}
       </Keyboard>
     </Container>
--- homeworks/abraham-trenyan/19-react-api/src/Components/Screen/Screen.jsx
+++ homeworks/abraham-trenyan/19-react-api/src/Components/Screen/Screen.jsx
@@ -1,8 +1,11 @@
-import './Screen.css'
-const Screen = ({value}) => {
-    return ( 
-        <div className="screen"><h2 className='screen__calc'>{value}</h2><h1 className='screen__result'>{value}</h1></div>
-     );
-}
- 
+import './Screen.css';
+const Screen = ({ value }) => {
+  return (
+    <div className="screen">
+      <h2 className="screen__calc">{value}</h2>
+      <h1 className="screen__result">{value.length > 0 ? value : 0}</h1>
+    </div>
+  );
+};
+
 export default Screen;
