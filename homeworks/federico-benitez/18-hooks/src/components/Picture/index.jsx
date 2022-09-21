8d7ebdef6ece1ac1f008b47b9bd26952a41a9280
Federico Benitez <fedebenitez1696@gmail.com>
2022-09-21 10:56:36 -0300

    feat: hover on cards

--- homeworks/federico-benitez/18-hooks/src/components/Picture/index.jsx
+++ homeworks/federico-benitez/18-hooks/src/components/Picture/index.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './style.module.css';
 
-
 function Picture({ url, isLoading }) {
+  const [isHover, setIsHover] = useState(false);
+
+  const onMouseEnter = () => setIsHover(true);
+  const onMouseLeave = () => setIsHover(false);
+
   return (
-    <div className={styles.card}>
-      {isLoading && (
+    <div className={styles.card} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
+      {(isLoading || isHover) && (
         <div className={styles.overlay}>
-          <img src="/001-refresh.svg" alt="reloading" className={styles.loading} />
+          <img src="/001-refresh.svg" alt="reloading" className={`${styles.loading} ${isLoading ? styles.spin : ''}`} />
         </div>
       )}
       <img src={url} className={styles.photo} alt="photo" />
