9d1db92bbb0ccdd6456fc240d8ceb2bd4af7d34a
Emiliano Acerbi <eacerbip@gmail.com>
2022-09-23 11:12:33 -0300

    added error component

--- homeworks/emiliano-acerbi/21-csr-ssr-ssg-nextjs/pages/index.js
+++ homeworks/emiliano-acerbi/21-csr-ssr-ssg-nextjs/pages/index.js
@@ -2,9 +2,20 @@ import useUsers from '../hooks/useUsers';
 import Container from '../components/Container';
 import Main from '../components/Main';
 import User from '../components/User';
-import FechButton from '../components/FechButton';
+import FetchButton from '../components/FetchButton';
 import Footer from '../components/Footer';
 import { useEffect } from 'react';
+import ErrorMessage from '../components/ErrorMessage';
+
+export async function getServerSideProps() {
+  const INITIAL_USERS = 5;
+  const URL = `https://tinyfac.es/api/data?limit=${INITIAL_USERS}&quality=0`;
+
+  const res = await fetch(URL);
+  const data = await res.json();
+
+  return { props: { data } };
+}
 
 export default function Home({ data }) {
   const { users, isRefreshing, setUsers, fetchSingleUser, refreshUser, refreshAllUsers } = useUsers();
@@ -17,11 +28,7 @@ export default function Home({ data }) {
 
   // If we have a problem with the first request then this will render
   if (data.error) {
-    return (
-      <Container>
-        <h1 className="error-title">There was a problem fetching the users, try again later.</h1>
-      </Container>
-    );
+    return <ErrorMessage />;
   }
 
   // If everything is correct this will render
@@ -31,19 +38,9 @@ export default function Home({ data }) {
         {users.map((user, index) => {
           return <User user={user} index={index} isRefreshing={isRefreshing} refreshUser={refreshUser} key={user.id} />;
         })}
-        <FechButton fetchSingleUser={fetchSingleUser} />
+        <FetchButton fetchSingleUser={fetchSingleUser} />
       </Main>
       <Footer refreshAllUsers={refreshAllUsers} />
     </Container>
   );
-}
-
-export async function getServerSideProps() {
-  const INITIAL_USERS = 5;
-  const URL = `https://tinyfac.es/api/data?limit=${INITIAL_USERS}&quality=0`;
-
-  const res = await fetch(URL);
-  const data = await res.json();
-
-  return { props: { data } };
 }
