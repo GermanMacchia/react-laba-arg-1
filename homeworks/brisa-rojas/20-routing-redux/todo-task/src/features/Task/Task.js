9418e67db0b16d6733469db3893fea097fd5fe3f
Brisa Rojas <brojas@solvd.com>
2022-09-23 18:26:52 -0300

    created to do task structure

---
+++ homeworks/brisa-rojas/20-routing-redux/todo-task/src/features/Task/Task.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  actions
+} from './taskSlice';
+import './styles.css';
+
+export function Task() { 
+    
+}
