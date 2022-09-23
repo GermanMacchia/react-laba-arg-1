237ec5bf870f76438cf009df200f8347a1d6cf8c
Emiliano Acerbi <eacerbip@gmail.com>
2022-09-23 14:32:59 -0300

    changed img to Image

--- homeworks/emiliano-acerbi/21-csr-ssr-ssg-nextjs/components/User/User.jsx
+++ homeworks/emiliano-acerbi/21-csr-ssr-ssg-nextjs/components/User/User.jsx
@@ -1,11 +1,19 @@
+import Image from 'next/image';
+
 function User({ user, index, isRefreshing, refreshUser }) {
   return (
     <div className="image" key={user.id} onClick={() => refreshUser(index)}>
       {/* Overylay */}
       <div className="image__overlay">
-        <img src="/001-refresh.svg" className={`${isRefreshing && 'image__animation'}`} alt="" />
+        <Image
+          src="/001-refresh.svg"
+          width={100}
+          height={100}
+          className={`${isRefreshing && 'image__animation'}`}
+          alt=""
+        />
       </div>
-      <img src={user.url} width={240} height={240} />
+      <Image src={user.url} width={240} height={240} alt={user.fist_name} />
     </div>
   );
 }
