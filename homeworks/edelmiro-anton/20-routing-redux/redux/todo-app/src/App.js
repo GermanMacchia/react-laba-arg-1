b295a0b7bb6dec2d35d353e5283046c9b0d4ef5f
Edelmiro Anton <anton.edelmiro@gmail.com>
2022-09-23 18:55:42 -0300

    main setup and styles

---
+++ homeworks/edelmiro-anton/20-routing-redux/redux/todo-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <div className="mainInput">
+        <input type="text" className="input_todoTask" placeholder="Create Todo-Task" />
+        <button className="button_add">Add</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
