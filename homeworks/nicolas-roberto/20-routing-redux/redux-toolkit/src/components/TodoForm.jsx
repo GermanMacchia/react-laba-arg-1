f4d9e546e9a3164b235578b24b4f947f1974d620
Nicolás <nicolaseugroberto@gmail.com>
2022-09-23 22:39:31 -0300

    Styling

--- homeworks/nicolas-roberto/20-routing-redux/redux-toolkit/src/components/TodoForm.jsx
+++ homeworks/nicolas-roberto/20-routing-redux/redux-toolkit/src/components/TodoForm.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './TodoForm.css';
 
 export default function TodoForm({ handleSubmit, todo, editId, setTodo }) {
   return (
--- homeworks/nicolas-roberto/20-routing-redux/redux-toolkit/src/components/TodoList.jsx
+++ homeworks/nicolas-roberto/20-routing-redux/redux-toolkit/src/components/TodoList.jsx
@@ -1,22 +1,19 @@
 import React from 'react';
-import editTodo from '../assets/write.svg';
-import deleteTodo from '../assets/delete.svg';
+import './TodoList.css';
 
 export default function TodoList({ todos, handleDelete, handleEdit }) {
   return (
-    <ul className="todo-list">
+    <ul>
       {todos.map((t) => (
-        <li key={t.id} className="todo-list_item">
-          <span className="todo-list_text" key={t.id}>
-            {t.todo}
-          </span>
-          <button className="todo-list_edit" onClick={() => handleEdit(t.id)}>
-            <img src={editTodo} alt="Edit" />
-          </button>
-          <button className="todo-list_delete" onClick={() => handleDelete(t.id)}>
-            <img src={deleteTodo} alt="Delete" />
-          </button>
-        </li>
+        <div className="todo-list">
+          <li key={t.id} className="todo-list_item">
+            <span className="todo-list_text" key={t.id}>
+              {t.todo}
+            </span>
+          </li>
+          <button className="todo-list_edit" onClick={() => handleEdit(t.id)}></button>
+          <button className="todo-list_delete" onClick={() => handleDelete(t.id)}></button>
+        </div>
       ))}
     </ul>
   );
