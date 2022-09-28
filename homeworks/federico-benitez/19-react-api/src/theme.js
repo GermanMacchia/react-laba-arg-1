c87135fbc972f722c33532ea2205c0d34170f7cc
Federico Benitez <fedebenitez1696@gmail.com>
2022-09-28 10:53:55 -0300

    feat: font family setted

--- homeworks/federico-benitez/19-react-api/src/theme.js
+++ homeworks/federico-benitez/19-react-api/src/theme.js
@@ -1,6 +1,9 @@
 import { extendTheme } from '@chakra-ui/react';
 
 const theme = extendTheme({
+  fonts:{
+    body: `'Poppins', sans-serif`
+  },
   colors: {
     background: {
       100: '#363746',
