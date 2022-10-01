6a72c7f6e64c26e5b5ce5c530c706c9c924156bd
dazuaje <davidazuaje1084@gmail.com>
2022-10-01 16:19:40 -0300

    Adding CSS

--- homeworks/david-azuaje/18-hooks/src/App.js
+++ homeworks/david-azuaje/18-hooks/src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
 import React, { useState } from 'react';
+import AddCard from './components/AddCard/AddCard';
+import Card from './components/Card/Card';
+import RefreshButton from './components/RefreshButton/RefreshButton';
 
 function App() {
 
 
   return (
-    <div className="App">
+    <div className="container">
+      <div className="container--card">
+
+        <AddCard />
+        <Card />
+      </div>
+      <RefreshButton />
 
     </div>
   );
