d227661f42bec847f009c4fda090467639e1c486
JulianOvie <julianovie234@gmail.com>
2022-10-02 12:29:13 -0300

    add logic components & styles

--- homeworks/julian-oviedo/19-react-api/calculator-app/src/App.jsx
+++ homeworks/julian-oviedo/19-react-api/calculator-app/src/App.jsx
@@ -1,9 +1,9 @@
-import './App.css';
+import Calculator from './components/Calculator/Calculator';
 
 function App() {
   return (
     <div className="App">
-      <h1>hello world</h1>
+      <Calculator />
     </div>
   );
 }
