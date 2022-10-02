5ef90ea32c988a24b122dc7318665c12679aca2f
Brisa Rojas <brojas@solvd.com>
2022-10-02 20:00:43 -0300

    adds test for use of calculator with the keyboard

--- homeworks/brisa-rojas/22-testing/src/components/Calculator/Calculator.test.js
+++ homeworks/brisa-rojas/22-testing/src/components/Calculator/Calculator.test.js
@@ -66,7 +66,8 @@ describe('Calculator renders correctly the result', () => {
     expect(result).toBe('2.5');
   });
 
-  test('5 % 3 = 2', () => { //to be 
+  test('5 % 3 = 2', () => {
+    //to be
     render(<Calculator />);
     fireEvent.click(document.querySelector('.button-number-5'));
     fireEvent.click(document.querySelector('.button-percent'));
@@ -105,5 +106,75 @@ describe('Calculator renders correctly the previous calculation', () => {
     fireEvent.click(document.querySelector('.button-equals'));
     const calculation = document.querySelector('.display__calculation').textContent;
     expect(calculation).toBe('5/2');
+  });
+});
+
+test('AC button clears the display', () => {
+  render(<Calculator />);
+  fireEvent.click(document.querySelector('.button-number-1'));
+  fireEvent.click(document.querySelector('.button-operator-divide'));
+  fireEvent.click(document.querySelector('.button-number-7'));
+  fireEvent.click(document.querySelector('.button-equals'));
+  fireEvent.click(document.querySelector('.button-clear'));
+  const result = document.querySelector('.display__result').textContent;
+  const prevCalc = document.querySelector('.display__calculation').textContent;
+  expect(result).toBe('') && expect(prevCalc).toBe('');
+});
+
+describe('Keyboard can be used to input numbers and operators', () => {
+  test('all numbers are shown in display after respective keydown events', () => {
+    render(<Calculator />);
+    const calc = document.querySelector('.calculator');
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].forEach((num) => {
+      fireEvent.keyDown(calc, { key: num });
+    });
+    const result = document.querySelector('.display__result').textContent;
+    expect(result).toBe('0123456789');
+  });
+
+  test('5.6*3.7 can be entered via keyboard', () => {
+    render(<Calculator />);
+    const calc = document.querySelector('.calculator');
+    [5, '.', 6, '*', 3, '.', 7].forEach((key) => {
+      fireEvent.keyDown(calc, { key });
+    });
+    const result = document.querySelector('.display__result').textContent;
+    expect(result).toBe('5.6*3.7');
+  });
+
+  test('Display is cleared after pressing c on keyboard', () => {
+    render(<Calculator />);
+    const calc = document.querySelector('.calculator');
+    [5, '.', 6, '*', 3, '.', 7].forEach((key) => {
+      fireEvent.keyDown(calc, { key });
+    });
+    fireEvent.keyDown(calc, { key: 'c' });
+    const result = document.querySelector('.display__result').textContent;
+    const prevCalc = document.querySelector('.display__calculation').textContent;
+    expect(result).toBe('') && expect(prevCalc).toBe('');
+  });
+
+  test('Display is cleared after pressing escape on keyboard', () => {
+    render(<Calculator />);
+    const calc = document.querySelector('.calculator');
+    [5, '.', 6, '*', 3, '.', 7].forEach((key) => {
+      fireEvent.keyDown(calc, { key });
+    });
+    fireEvent.keyDown(calc, { key: 'Escape' });
+    const result = document.querySelector('.display__result').textContent;
+    const prevCalc = document.querySelector('.display__calculation').textContent;
+    expect(result).toBe('') && expect(prevCalc).toBe('');
+  });
+
+  test('one digit can be deleted after pressing backspace on keyboard', () => {
+    render(<Calculator />);
+    const calc = document.querySelector('.calculator');
+    [1, 7, '.', 0, 3, '*', 2, 0, 0, 0].forEach((key) => {
+      fireEvent.keyDown(calc, { key });
+    });
+    const firstCalc = document.querySelector('.display__result').textContent;
+    fireEvent.keyDown(calc, { key: 'Backspace' });
+    const secondCalc = document.querySelector('.display__result').textContent;
+    expect(secondCalc).toBe(firstCalc.slice(0, -1));
   });
 });
